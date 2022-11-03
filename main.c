13191e6a7c355df9e7feb64bd2bc13a63c9f5315
anselnettles <aviholai@student.hive.fi>
2022-11-03 17:38:44 +0200

    Practice of use of RenderClear(), SetRenderDrawColor() and CreateRender()

--- main.c
+++ main.c
@@ -6,44 +6,41 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 18:24:05 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/03 15:34:39 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/03 17:20:14 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include "libSDL2/include/SDL2/SDL.h"
+#include "library.h"
 
-#define WINDOW 480
-
-int	main()
+int	main(void)
 {
-	write(1, "Crossroads of my programming life.", 34);
 	SDL_Event		event;
 	SDL_Renderer	*renderer;
 	SDL_Window		*win;
 	int				i;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	win = SDL_CreateWindow("Game.", 0, 0, WINDOW, WINDOW, 0);
+	win = SDL_CreateWindow(TITLE, 0, 0, WIDTH, HEIGHT, 0);
 	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-	SDL_RenderClear(renderer);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
 	i = 200;
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 	while (i)
 	{
 		SDL_RenderDrawPoint(renderer, i, i);
 		i--;
 	}
 	SDL_RenderPresent(renderer);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 	while (1)
 	{
 		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-			break;
+			break ;
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
 	return (0);
 }
-
