7b8dc1e8f214ddb376daa6f73a33d78db671d06c
anselnettles <aviholai@student.hive.fi>
2022-11-04 21:19:07 +0200

    Two error management checks. Probably need to move to another function.

--- src/parse_level.c
+++ src/parse_level.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 18:33:26 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/04 19:01:28 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/04 21:14:23 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,20 @@
 
 int	read_file(t_system *system, t_editor *editor)
 {
+	int	i;
+
 	if (system->user_request)
-		printf("yeehaaw. ");
-	if (editor->file)
-		printf("hot dang. ");
+		// to inform what the use is.
+	if (!(editor->file))
+		return(error(NO_FILE));
+	i = 0;
+	while (editor->file[i++])
+	{
+		if (editor->file[i] == '.' && editor->file[i + 1] == 'd'
+				&& editor->file[i + 2] == 'n' && !(editor->file[i + 3]))
+			break ;
+		if (!(editor->file[i]))
+			return (error(BAD_FILENAME));
+	}
 	return (0);
 }
