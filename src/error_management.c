b1cc23ec87a8214664f470d043501b7a238d96c0
anselnettles <aviholai@student.hive.fi>
2022-11-04 21:14:57 +0200

    New error types added: No file, Bad filename.

--- src/error_management.c
+++ src/error_management.c
@@ -6,12 +6,12 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:40:37 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/04 18:05:28 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/04 21:00:11 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
-#define NUMBER 1
+#define NUMBER 3
 
 int	error(int code)
 {
@@ -19,6 +19,8 @@ int	error(int code)
 
 	error_message[0] = T_ORANGE "Usage: " T_NUL "./" NAME
 		T_ORANGE " | " T_NUL "./" NAME T_RED " <file.dn>" T_NUL;
+	error_message[1] = T_RED "Error: No file applied for editor." T_NUL;
+	error_message[2] = T_RED "Error: Not a '.dn' level file." T_NUL;
 	write(1, error_message[code], ft_strlen(error_message[code]));
 	return (1);
 }
