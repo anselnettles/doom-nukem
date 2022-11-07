0f18b41bd3647d3ea70e6a39e49ae545e814fdfd
anselnettles <aviholai@student.hive.fi>
2022-11-07 10:57:40 +0200

    Small layout readjustment.

--- src/library.h
+++ src/library.h
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:26:57 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/06 16:29:59 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/07 10:32:33 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,9 @@
 # define RUN_GAME 1						// User request for running the game.
 # define RUN_LEVEL_EDITOR 2				// User request for running the editor.
 # define ERROR 1						// Reference to return value.
-# define MAX_READ 5000
+# define MAX_READ 5000					// Irrelevant level size maximum.
 
+//Terminal output color codes.
 # define T_NUL "\033[0m"				// Default terminal type color.
 # define T_ORANGE "\033[1;33m"			// Bold orange terminal type color.
 # define T_RED "\033[0;31m"				// A red terminal type color.
@@ -59,7 +60,7 @@ typedef enum e_error
 
 int		error(int code);				// Error management function.
 int		main(int argc, char **argv);	// The main function.
-int		read_file(t_system *system, t_editor *editor);
+int		read_file(t_system *system, t_editor *editor);	// Level file reader.
 
 size_t	ft_strlen(const char *s);		// Measure the length of applied string.
 
