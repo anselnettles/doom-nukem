2d04ab2bb43ae2b258b0b0d0ac55021025539d0d
anselnettles <aviholai@student.hive.fi>
2022-11-08 17:36:24 +0200

    Applied use of SDL error code. SDL hooks set for an independent function. Considering what's the use of SDL_Init.

--- src/graphic_render.c
+++ src/graphic_render.c
@@ -6,16 +6,28 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:53:39 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/08 16:56:25 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:30:17 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
-void	initialize(t_graph graph)
+int	initialize(t_graph graph)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	//SDL_Init(SDL_INIT_VIDEO);
 	graph.win = SDL_CreateWindow(TITLE, 0, 0, WIDTH, HEIGHT, 0);
+	if (graph.win == NULL)
+		return (ERROR);
+	return (0);
+}
+
+void	SDL_loop(t_graph graph)
+{
+	while (ENDLESS)
+	{
+		if (SDL_PollEvent(&graph.e) && graph.e.type == SDL_QUIT)
+			break ;
+	}
 }
 
 int	render(t_system *system, t_editor *editor, t_index *index)
@@ -23,18 +35,14 @@ int	render(t_system *system, t_editor *editor, t_index *index)
 	t_graph	graph;
 
 	ft_bzero(&graph, sizeof(t_graph));
-	initialize(graph);
-	while (1)
-	{
-		if (SDL_PollEvent(&graph.e) && graph.e.type == SDL_QUIT)
-			break ;
-	}
-	SDL_DestroyRenderer(graph.ren);
-	SDL_DestroyWindow(graph.win);
-	SDL_Quit();
+	if (initialize(graph) == ERROR)
+		return (error(SDL_FAIL));
 	if (system && editor && index)
 	{
 		//
 	}
+	SDL_loop(graph);
+	SDL_DestroyWindow(graph.win);
+	SDL_Quit();
 	return (0);
 }
