d0fb999c251b8d53aa6facb22c2f4e822c7a3fbf
anselnettles <aviholai@student.hive.fi>
2022-11-08 17:13:02 +0200

    The color commands are causing an overflow.

--- src/level_editor.c
+++ src/level_editor.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:56:40 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/08 14:21:33 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:10:26 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 void	print_array(t_editor *editor, t_index *index)
 {
 	index->i = 0;
-	write(1, T_ORANGES, 10);
+	//write(1, T_RED, 10);
 	while (index->i != index->y)
 	{
 		write(1, editor->array[index->i], (size_t)index->width);
 		write(1, "\n", 1);
 		index->i++;
 	}
-	write(1, T_NUL, 7);
+	//write(1, T_NUL, 7);
 }
 
 int	editor_sequence(t_system *system, t_editor *editor, t_index *index)
 {
 	print_array(editor, index);
-
+	if (render(system, editor, index) == ERROR)
+		return (ERROR);
 	if (system->user_request)
 	{
 		//
