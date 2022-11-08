ae7dfad387f2b175a0815c17f10c71bd2b29ae96
anselnettles <aviholai@student.hive.fi>
2022-11-08 21:09:12 +0200

    Dereference fixes to all the graphic functions. Have to fix struct calls tomorrow.

--- src/graphic_render.c
+++ src/graphic_render.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:53:39 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/08 20:04:37 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/08 21:05:52 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,43 +18,43 @@ void	pixel_put(SDL_Surface *surf, int x, int y, uint32_t color)
 	((uint32_t *)surf->pixels)[x + (y * WIDTH)] = color;
 }
 
-int	render(t_graph graph, t_editor *editor, t_index *index)
+int	render(t_graph *graph, t_editor *editor, t_index *index)
 {
-	graph.x = WIDTH / 2;
-	graph.y = HEIGHT / 2;
-	graph.color = 0xFFFFFF;
-	pixel_put(graph.surf, graph.x, graph.y, graph.color);
-	SDL_UpdateWindowSurface(graph.win);
-	if (editor)
-		;
-	if (index)
-		;
+	graph->x = WIDTH / 2;
+	graph->y = HEIGHT / 2;
+	graph->color = 0xFFFFFF;
+	pixel_put(graph->surf, graph->x, graph->y, graph->color);
+	SDL_UpdateWindowSurface(graph->win);
+	(void)editor;
+	(void)index;
 	return (0);
 }
 
-int	initialize(t_graph graph)
+int	initialize(t_graph *graph)
 {
-	//SDL_Init(SDL_INIT_VIDEO);
-	graph.win = SDL_CreateWindow(TITLE, 0, 0, WIDTH, HEIGHT, 0);
-	if (graph.win == NULL)
+	SDL_Init(SDL_INIT_VIDEO);
+
+	graph->win = SDL_CreateWindow(TITLE, 0, 0, WIDTH, HEIGHT, 0);
+	if (graph->win == NULL)
 		return (ERROR);
-	graph.surf = SDL_GetWindowSurface(graph.win);
-	if (graph.surf == NULL)
+	graph->surf = SDL_GetWindowSurface(graph->win);
+	if (graph->surf == NULL)
 		return (ERROR);
-	graph.surf->pixels = ft_memalloc(sizeof(WIDTH * HEIGHT));
+	//ft_bzero(graph.surf->pixels);
+	//graph.surf->pixels = ft_memalloc(sizeof(WIDTH * HEIGHT));
 	return (0);
 }
 
-void	SDL_loop(t_graph graph)
+void	SDL_loop(t_graph *graph)
 {
 	while (ENDLESS)
 	{
-		if (SDL_PollEvent(&graph.e) && graph.e.type == SDL_QUIT)
+		if (SDL_PollEvent(&graph->e) && graph->e.type == SDL_QUIT)
 			break ;
 	}
 }
 
-int	graphic_interface(t_graph graph, t_system *system, t_editor *editor, t_index *index)
+int	graphic_interface(t_graph *graph, t_system *system, t_editor *editor, t_index *index)
 {
 	//t_graph	graph;
 
@@ -62,10 +62,9 @@ int	graphic_interface(t_graph graph, t_system *system, t_editor *editor, t_index
 		return (error(SDL_FAIL));
 	if (render(graph, editor, index) == ERROR)
 		return (error(RENDER_FAIL));
-	if (system && editor && index)
-	{
-		//
-	}
+	(void)system;
+	(void)editor;
+	(void)index;
 	SDL_loop(graph);
 	//SDL_DestroyWindow(graph.win);
 	//SDL_Quit();
