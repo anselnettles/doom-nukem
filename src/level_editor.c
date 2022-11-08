097c406ed13b540c1ec530794e32a3b73aac57f4
anselnettles <aviholai@student.hive.fi>
2022-11-08 21:09:53 +0200

    Fixed dereferences for graphic calls. Will fix structs tomorrow.

--- src/level_editor.c
+++ src/level_editor.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:56:40 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/08 19:35:35 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/08 21:00:06 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	print_array(t_editor *editor, t_index *index)
 	//write(1, T_NUL, 7);
 }
 
-int	editor_sequence(t_graph graph, t_system *system, t_editor *editor, t_index *index)
+int	editor_sequence(t_graph *graph, t_system *system, t_editor *editor, t_index *index)
 {
 	print_array(editor, index);
 	if (render(graph, editor, index) == ERROR)
