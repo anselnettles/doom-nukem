9ea494e6d3ce74e8a1092ca92dd631232867eeb5
anselnettles <aviholai@student.hive.fi>
2022-11-08 20:07:18 +0200

    Work on all the functions: graphic_interface(), SDL_loop(), initialize(), render(), pixel_put()

--- src/graphic_render.c
+++ src/graphic_render.c
@@ -6,11 +6,31 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:53:39 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/08 17:30:17 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/08 20:04:37 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
+
+void	pixel_put(SDL_Surface *surf, int x, int y, uint32_t color)
+{
+//	printf("pixel: %d \n", ((uint32_t *)surf->pixels[x + (y * WIDTH)]));
+	((uint32_t *)surf->pixels)[x + (y * WIDTH)] = color;
+}
+
+int	render(t_graph graph, t_editor *editor, t_index *index)
+{
+	graph.x = WIDTH / 2;
+	graph.y = HEIGHT / 2;
+	graph.color = 0xFFFFFF;
+	pixel_put(graph.surf, graph.x, graph.y, graph.color);
+	SDL_UpdateWindowSurface(graph.win);
+	if (editor)
+		;
+	if (index)
+		;
+	return (0);
+}
 
 int	initialize(t_graph graph)
 {
@@ -18,6 +38,10 @@ int	initialize(t_graph graph)
 	graph.win = SDL_CreateWindow(TITLE, 0, 0, WIDTH, HEIGHT, 0);
 	if (graph.win == NULL)
 		return (ERROR);
+	graph.surf = SDL_GetWindowSurface(graph.win);
+	if (graph.surf == NULL)
+		return (ERROR);
+	graph.surf->pixels = ft_memalloc(sizeof(WIDTH * HEIGHT));
 	return (0);
 }
 
@@ -30,19 +54,20 @@ void	SDL_loop(t_graph graph)
 	}
 }
 
-int	render(t_system *system, t_editor *editor, t_index *index)
+int	graphic_interface(t_graph graph, t_system *system, t_editor *editor, t_index *index)
 {
-	t_graph	graph;
+	//t_graph	graph;
 
-	ft_bzero(&graph, sizeof(t_graph));
 	if (initialize(graph) == ERROR)
 		return (error(SDL_FAIL));
+	if (render(graph, editor, index) == ERROR)
+		return (error(RENDER_FAIL));
 	if (system && editor && index)
 	{
 		//
 	}
 	SDL_loop(graph);
-	SDL_DestroyWindow(graph.win);
-	SDL_Quit();
+	//SDL_DestroyWindow(graph.win);
+	//SDL_Quit();
 	return (0);
 }
