32fa2dda3495bd13a60a5ace836953bf38d6dfbd
anselnettles <aviholai@student.hive.fi>
2022-11-09 16:49:12 +0200

    Simple key polling function. Quits when ESC.

--- src/controls_keyboard.c
+++ src/controls_keyboard.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:16:37 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/09 14:48:52 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/09 16:47:11 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 void	keyboard(t_graph *graph)
 {
-	while (SDL_PollEvent(e))
+	while (SDL_PollEvent(&graph->e))
 	{
-
+		if (graph->e.type == SDL_KEYDOWN)
+		{
+			if (graph->e.key.keysym.sym == SDLK_ESCAPE)
+			{
+				SDL_DestroyWindow(graph->win);
+				SDL_Quit();
+				exit(0);
+			}
+		}
 	}
 }
