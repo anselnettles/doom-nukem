2faf2a01908c8f46128fa606b01d29abf239ad8d
anselnettles <aviholai@student.hive.fi>
2022-11-09 14:54:04 +0200

    Initial utilities_graphic.c

---
+++ src/utilities_graphic.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilities_graphic.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 13:35:22 by aviholai          #+#    #+#             */
+/*   Updated: 2022/11/09 13:58:47 by aviholai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "library.h"
+
+//A pixel drawing function for the SDL surface, created to make the rendering
+//process more simpler.
+void	pixel_put(SDL_Surface *surf, int x, int y, uint32_t color)
+{
+	((uint32_t *)surf->pixels)[x + (y * WIDTH)] = color;
+}
+
+//'sdl_loop()' keeps Simple Direct MediaLayer's PollEvent consistently running.
+void	sdl_loop(t_graph *graph)
+{
+	while (ENDLESS)
+	{
+		if (SDL_PollEvent(&graph->e) && graph->e.type == SDL_QUIT)
+			break ;
+	}
+}
