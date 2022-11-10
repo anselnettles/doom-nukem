99b52b0f94f78362abb5df23ae5747c1f92b72d8
anselnettles <aviholai@student.hive.fi>
2022-11-10 17:35:26 +0200

    Keyboard control for resolution scaling.

--- src/controls_keyboard.c
+++ src/controls_keyboard.c
@@ -6,24 +6,36 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:16:37 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/09 16:55:59 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:26:37 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
-void	keyboard(t_graph *graph)
+void	keyboard(t_rain *r)
 {
-	while (SDL_PollEvent(&graph->e))
+	while (SDL_PollEvent(&r->graph.e))
 	{
-		if (graph->e.type == SDL_KEYDOWN)
+		if (r->graph.e.type == SDL_KEYDOWN)
 		{
-			if (graph->e.key.keysym.sym == ESC)
+			if (r->graph.e.key.keysym.sym == ESC)
 			{
-				SDL_DestroyWindow(graph->win);
+				SDL_DestroyWindow(r->graph.win);
 				SDL_Quit();
 				exit(0);
 			}
+			if (r->graph.e.key.keysym.sym == NUMPAD_MINUS)
+			{
+				if (r->graph.scaler == 1)
+					r->graph.scaler++;
+				else if (r->graph.scaler == 2)
+					r->graph.scaler--;
+				r->graph.width = WIDTH * r->graph.scaler;
+				r->graph.height = HEIGHT * r->graph.scaler;
+				SDL_SetWindowSize(r->graph.win, r->graph.width,
+						r->graph.height);
+			}
 		}
+		render(r);
 	}
 }
