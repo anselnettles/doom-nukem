8bbd8214aa809e4ff53c3b72f8d6ef19abebf7da
Ansel Ami Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2022-11-16 16:24:58 +0200

    Added whitespace to the end of line.

--- src/error_management.c
+++ src/error_management.c
@@ -18,18 +18,18 @@ int	error(int code)
 	const char	*error_message[NUMBER];
 
 	error_message[0] = T_ORANGE "Usage: " T_NUL "./" NAME
-		T_ORANGE " | " T_NUL "./" NAME T_RED " <file.dn>" T_NUL;
-	error_message[1] = T_RED "Error: No file applied for editor." T_NUL;
-	error_message[2] = T_RED "Error: Not a valid (.dn) level file." T_NUL;
-	error_message[3] = T_RED "Error: File open failure. File not found." T_NUL;
-	error_message[4] = T_RED "Error: File read failure." T_NUL;
-	error_message[5] = T_RED "Error: File too large." T_NUL;
-	error_message[6] = T_RED "Error: File close failure." T_NUL;
-	error_message[7] = T_RED " is an illegal character in level file." T_NUL;
-	error_message[8] = T_RED "Error: File width inconsistent." T_NUL;
-	error_message[9] = T_RED "Error: Level editor failure." T_NUL;
-	error_message[10] = T_RED "Error: Simple DirectMedia Layer failure." T_NUL;
-	error_message[11] = T_RED "Error: Graphics rendering failure." T_NUL;
+		T_ORANGE " | " T_NUL "./" NAME T_RED " <file.dn> " T_NUL;
+	error_message[1] = T_RED "Error: No file applied for editor. " T_NUL;
+	error_message[2] = T_RED "Error: Not a valid (.dn) level file. " T_NUL;
+	error_message[3] = T_RED "Error: File open failure. File not found. " T_NUL;
+	error_message[4] = T_RED "Error: File read failure. " T_NUL;
+	error_message[5] = T_RED "Error: File too large. " T_NUL;
+	error_message[6] = T_RED "Error: File close failure. " T_NUL;
+	error_message[7] = T_RED " is an illegal character in level file. " T_NUL;
+	error_message[8] = T_RED "Error: File width inconsistent. " T_NUL;
+	error_message[9] = T_RED "Error: Level editor failure. " T_NUL;
+	error_message[10] = T_RED "Error: Simple DirectMedia Layer failure. " T_NUL;
+	error_message[11] = T_RED "Error: Graphics rendering failure. " T_NUL;
 	write(1, error_message[code], ft_strlen(error_message[code]));
 	return (ERROR);
 }
