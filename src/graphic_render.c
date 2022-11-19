55599349eb53c2a1b113bcd635ad76468f1682c5
Ansel "Ami" Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2022-11-19 10:11:15 +0200

    Moved map draw_slot() functions to a distinct file.

--- src/graphic_render.c
+++ src/graphic_render.c
@@ -5,103 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 16:53:39 by aviholai          #+#    #+#             */
+/*   Created: 2022/11/19 10:05:39 by aviholai          #+#    #+#             */
 /*   Updated: 2022/11/11 18:23:17 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
-static int	draw_slot_outline(t_rain *r)
-{
-	pixel_put(&r->graph, r->graph.x - 1, r->graph.y - 1, SLOT);
-	pixel_put(&r->graph, r->graph.x, r->graph.y - 1, SLOT);
-	pixel_put(&r->graph, r->graph.x + 2, r->graph.y - 1, SLOT);
-	pixel_put(&r->graph, r->graph.x + 3, r->graph.y - 1, SLOT);
-	pixel_put(&r->graph, r->graph.x - 1, r->graph.y, SLOT);
-	pixel_put(&r->graph, r->graph.x + 3, r->graph.y, SLOT);
-	pixel_put(&r->graph, r->graph.x - 1, r->graph.y + 2, SLOT);
-	pixel_put(&r->graph, r->graph.x + 3, r->graph.y + 2, SLOT);
-	pixel_put(&r->graph, r->graph.x - 1, r->graph.y + 3, SLOT);
-	pixel_put(&r->graph, r->graph.x, r->graph.y + 3, SLOT);
-	pixel_put(&r->graph, r->graph.x + 2, r->graph.y + 3, SLOT);
-	pixel_put(&r->graph, r->graph.x + 3, r->graph.y + 3, SLOT);
-	return (0);
-}
-
-static int	draw_map_tile(t_rain *r, int color)
-{
-	pixel_put(&r->graph, r->graph.x, r->graph.y, color);
-	pixel_put(&r->graph, r->graph.x + 1, r->graph.y, color);
-	pixel_put(&r->graph, r->graph.x + 2, r->graph.y, color);
-	pixel_put(&r->graph, r->graph.x, r->graph.y + 1, color);
-	pixel_put(&r->graph, r->graph.x + 1, r->graph.y + 1, color);
-	pixel_put(&r->graph, r->graph.x + 2, r->graph.y + 1, color);
-	pixel_put(&r->graph, r->graph.x, r->graph.y + 2, color);
-	pixel_put(&r->graph, r->graph.x + 1, r->graph.y + 2, color);
-	pixel_put(&r->graph, r->graph.x + 2, r->graph.y + 2, color);
-	return (0);
-}
-
-static int	draw_pl_tile(t_rain *r, char a[MAX + 1][MAX + 1], int x, int y)
-{
-	if (a[y][x] == '#' || a[y + 1][x] == '#' || a[y][x + 1] == '#'
-			|| a[y +1][x + 1] == '#' || a[y][x] == '/'
-			|| a[y + 1][x] == '/' || a[y][x + 1] == '/'
-			|| a[y + 1][x + 1] == '/' || a[y][x] == '\\'
-			|| a[y + 1][x] == '\\' || a[y][x + 1] == '\\'
-			|| a[y + 1][x + 1] == '\\' || a[y][x] == '*'
-			|| a[y + 1][x] == '*' || a[y][x + 1] == '*'
-			|| a[y + 1][x + 1] == '*')
-		draw_map_tile(r, WALL);
-	else
-		draw_map_tile(r, EMPTY);
-	return (0);
-}
-
-static int	draw_dev_tile(t_rain *r, char a[MAX + 1][MAX + 1], int x, int y)
-{
-	if (a[y][x] == '#' || a[y][x] == '/' || a[y][x] == '\\'
-			|| a[y][x] == '*')
-		draw_map_tile(r, WALL);
-	else if (a[y][x] == '0')
-		draw_map_tile(r, PIT);
-	else if (a[y][x] >= '1' && a[y][x] <= '9')
-		draw_map_tile(r, FLOOR);
-	else if (a[y][x] == 'a')
-		draw_map_tile(r, SKY);	
-	else if (a[y][x] >= 'b' && a[y][x] <= 'j')
-		draw_map_tile(r, CEILING);
-	else
-		draw_map_tile(r, EMPTY);
-	return (0);
-}
-
 static int	draw_arraymap(t_rain *r)
 {
 	int	x;
 	int	y;
 
-	if (r->editor.array[0] != NULL) //how to make this check more useful?
+	if (r->editor.array[0] != NULL)
 	{
 		r->graph.y = TOP_MARGIN;
-		r->graph.x = MAP_MARGIN;			// default location for the map?
+		r->graph.x = MAP_MARGIN;
 		x = 1;
 		y = 0;
 		while (y <= r->index.y)
 		{
 			while (x <= r->index.width)
-			{
-				if (r->graph.map == PLAYER_MAP)
-					draw_pl_tile(r, (char **)r->editor.array, x, y);
-				else if (r->graph.map == DEV_MAP)
-					draw_dev_tile(r, (char **)r->editor.array, x, y);
-				draw_slot_outline(r);
-				x++;
-				if (r->graph.map == PLAYER_MAP)
-					x++;
-				r->graph.x += 6;
-			}
+				x = draw_map_slot(r, x, y);
 			x = 1;
 			r->graph.x = MAP_MARGIN;
 			y++;
