7877d9ddc93815dcf2967aa3f15939a73083082c
anselnettles <aviholai@student.hive.fi>
2022-11-21 12:14:04 +0200

    Fixed to apply to school norm.

--- src/level_editor.c
+++ src/level_editor.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:56:40 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/10 17:26:45 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/21 12:11:08 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	print_array(t_editor *editor, t_index *index)
 {
 	index->i = 0;
-	//write(1, T_RED, 10);
 	while (index->i != index->y)
 	{
 		write(1, editor->array[index->i], (size_t)index->width);
 		write(1, "\n", 1);
 		index->i++;
 	}
-	//write(1, T_NUL, 7);
 }
 
 int	editor_sequence(t_rain *rain)
