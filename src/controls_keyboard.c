48c004badbc71e097bf42efebcb9f7d80b56c1de
anselnettles <aviholai@student.hive.fi>
2022-11-21 12:13:19 +0200

    Divided controls to different key functions. Norm adjusts.

--- src/controls_keyboard.c
+++ src/controls_keyboard.c
@@ -6,42 +6,47 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:16:37 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/11 17:40:27 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/21 12:08:27 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
+static void	key_minus(t_rain *r)
+{
+	if (r->graph.scale == 1)
+		r->graph.scale = 2;
+	else
+		r->graph.scale = 1;
+	r->graph.width = (WIDTH * r->graph.scale);
+	r->graph.height = (HEIGHT * r->graph.scale);
+	SDL_SetWindowSize(r->graph.win, r->graph.width, r->graph.height);
+	r->graph.surf = SDL_GetWindowSurface(r->graph.win);
+}
+
+static void	key_esc(t_rain *r)
+{
+	SDL_DestroyWindow(r->graph.win);
+	SDL_Quit();
+	exit(0);
+}
+
 void	keyboard(t_rain *r)
 {
-		if (r->graph.e.type == SDL_KEYDOWN)
+	if (r->graph.e.type == SDL_KEYDOWN)
+	{
+		write(1, ".", 1);
+		if (r->graph.e.key.keysym.sym == ESC)
+			key_esc(r);
+		if (r->graph.e.key.keysym.sym == NUMPAD_MINUS)
+			key_minus(r);
+		if (r->graph.e.key.keysym.sym == SDLK_m)
 		{
-			write(1, ".", 1);
-			if (r->graph.e.key.keysym.sym == ESC)
-			{
-				SDL_DestroyWindow(r->graph.win);
-				SDL_Quit();
-				exit(0);
-			}
-			if (r->graph.e.key.keysym.sym == NUMPAD_MINUS)
-			{
-				if (r->graph.scale == 1)
-					r->graph.scale = 2;
-				else
-					r->graph.scale = 1;
-				r->graph.width = (WIDTH * r->graph.scale);
-				r->graph.height = (HEIGHT * r->graph.scale);
-				SDL_SetWindowSize(r->graph.win, r->graph.width,
-						r->graph.height);
-				r->graph.surf = SDL_GetWindowSurface(r->graph.win);
-			}
-			if (r->graph.e.key.keysym.sym == SDLK_m)
-			{
-				if (r->graph.map == PLAYER_MAP)
-					r->graph.map = DEV_MAP;
-				else
-					r->graph.map = PLAYER_MAP;
-			}
-			render(r);
+			if (r->graph.map == PLAYER_MAP)
+				r->graph.map = DEV_MAP;
+			else
+				r->graph.map = PLAYER_MAP;
 		}
+		render(r);
+	}
 }
