a70496b6ae3b8c33dd16b90418aab8fd52e4d156
anselnettles <aviholai@student.hive.fi>
2022-11-30 10:55:35 +0200

    Fixed the header file to apply to school norm.

--- src/library.h
+++ src/library.h
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:26:57 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/21 12:12:23 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/30 10:54:25 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 # define LIBRARY_H
 
 /*LIBRARIES.*/
-# include <unistd.h>				/*UNIX Standard library.*/
-# include <stdlib.h>				/*Standard library.*/
-# include <fcntl.h>				/*File Control library.*/
+# include <unistd.h>						/*UNIX Standard library.*/
+# include <stdlib.h>						/*Standard library.*/
+# include <fcntl.h>							/*File Control library.*/
 # include "../libSDL2/include/SDL2/SDL.h"	/*Simple DirectMedia Layer.*/
 
 /*GRAPHIC GLOBAL DEFINITIONS*/
 # define TITLE "Bitter Cold Droplets in Autumn Rain | github.com/AnselNettles"
-# define NAME "doom-nukem"	/*Rename when necessary.*/
-# define WIDTH 640		/*Window resolution width.*/
-# define HEIGHT 480		/*Window resolution height.*/
-# define TOP_MARGIN 10		/*Top margin length for the UI.*/
-# define MAP_MARGIN 460		/*Width margin distance for the map UI.*/
+# define NAME "doom-nukem"			/*Rename when necessary.*/
+# define WIDTH 640					/*Window resolution width.*/
+# define HEIGHT 480					/*Window resolution height.*/
+# define TOP_MARGIN 10				/*Top margin length for the UI.*/
+# define MAP_MARGIN 460				/*Width margin distance for the map UI.*/
 
 /*SYSTEM MECHANICS GLOBAL DEFINITIONS*/
-# define RUN_GAME 1			// User request for running the game.
-# define RUN_LEVEL_EDITOR 2		// User request for running the editor.
-# define TRUE 1				// Set to true.
-# define FALSE 0			// Set to false.
-# define ERROR 1			// Reference to return value.
-# define SCALE 1			// Resolution scale.
-# define SDL_ERROR -1			// Reference to SDL function's return.
-# define MAX 1000			// Irrelevant level size maximum.
-# define ENDLESS 1			// SDL loop.
-# define PLAYER_MAP 1			// Block map definition.
-# define DEV_MAP 2			// Array map definition.
+# define RUN_GAME 1				/*User request for running the game.*/
+# define RUN_LEVEL_EDITOR 2		/*User request for running the editor.*/
+# define TRUE 1					/*Set to true.*/
+# define FALSE 0				/*Set to false.*/
+# define ERROR 1				/*Reference to return value.*/
+# define SCALE 1				/*Resolution scale.*/
+# define SDL_ERROR -1			/*Reference to SDL function's return.*/
+# define MAX 1000				/*Irrelevant level size maximum.*/
+# define ENDLESS 1				/*SDL loop.*/
+# define PLAYER_MAP 1			/*Block map definition.*/
+# define DEV_MAP 2				/*Array map definition.*/
 
 /*CONTROL MAPPING GLOBAL DEFINITIONS*/
-# define ESC SDLK_ESCAPE		/*SDL Keysym definition for ESC.*/
+# define ESC SDLK_ESCAPE			/*SDL Keysym definition for ESC.*/
 # define NUMPAD_PLUS SDLK_PLUS		/*SDL Keysym definition for plus.*/
 # define NUMPAD_MINUS SDLK_MINUS	/*SDL Keysym definition for minus.*/
 
-//Map output color codes.
+/*ARRAY MAP COLOR DEFINITIONS*/
 # define WALL 0xFF772E
 # define SKY 0x004744
 # define CEILING 0x11736E
@@ -54,28 +54,28 @@
 # define EMPTY 0x171717
 # define SLOT 0x303030 
 
-//Terminal output color codes.
-# define T_NUL "\033[0m"		// Default terminal type color.
-# define T_ORANGE "\033[1;33m"		// Bold orange terminal type color.
-# define T_ORANGES "\033[0;33m"		// A slim orange terminal type color.
-# define T_RED "\033[0;31m"		// A red terminal type color.
-# define T_LGRAY "\033[0;37m"		// A light gray terminal type color.
+/*TERMINAL OUTPUT COLOR DEFINITIONS*/
+# define T_NUL "\033[0m"			/*Default terminal type color.*/
+# define T_ORANGE "\033[1;33m"		/*Bold orange terminal type color.*/
+# define T_ORANGES "\033[0;33m"		/*A slim orange terminal type color.*/
+# define T_RED "\033[0;31m"			/*A red terminal type color.*/
+# define T_LGRAY "\033[0;37m"		/*A light gray terminal type color.*/
 
-//System-wise variables for run and check-up calls through the two programs.
+/*System-wise variables for run and check-up calls through the two programs.*/
 typedef struct s_system {
 	int		user_request;
 }	t_system;
 
-//Editor-wise variables used mainly for resolving the level editor program.
+/*Editor-wise variables used mainly for resolving the level editor program.*/
 typedef struct s_editor {
 	char		*file;
 	char		buffer[MAX + 1];
 	char		array[MAX + 1][MAX + 1];
 }	t_editor;
 
-//Index-wise variables used for counts. Index 'i' is used for the level file's
-//buffer string, while variables 'x' and 'y' are for the array of the extracted
-//level.
+/*Index-wise variables used for counts. Index 'i' is used for the level file's
+buffer string, while variables 'x' and 'y' are for the array of the extracted
+level.*/
 typedef struct s_index {
 	int			i;
 	int			x;
@@ -83,20 +83,21 @@ typedef struct s_index {
 	int			width;
 }	t_index;
 
-/* Player location struct. */
+/*Player location struct.*/
 typedef struct s_player {
-	struct	xyz { float	x;
+	struct	s_xyz {
+		float	x;
 		float	y;
-		float	z; } where,
-		    velocity;
-	float	angle;
-	float	anglesin;
-	float	anglecos;
-	float	yaw;
-	unsigned	sector;
+		float	z;
+	} where, velocity;
+	float			angle;
+	float			anglesin;
+	float			anglecos;
+	float			yaw;
+	unsigned int	sector;
 }	t_player;
 
-/* Graphical-wise variables used for SDL and graphical drawing. */
+/*Graphical-wise variables used for SDL and graphical drawing.*/
 typedef struct s_graph {
 	SDL_Event		e;
 	SDL_Window		*win;
@@ -111,13 +112,15 @@ typedef struct s_graph {
 	int				map;
 	float			floor;
 	float			ceiling;
-	struct	xy { float x;
-		float	y; } *vertex;
-	signed char	*neighbours;
-	unsigned	npoints;
+	struct s_xy {
+		float	x;
+		float	y;
+	} *vertex;
+	signed char		*neighbours;
+	unsigned int	npoints;
 }	t_graph;
 
-//We're making a mother of all structs here, Jack. Can't fret over every rule.
+/*Mother struct*/
 typedef struct s_rain {
 	t_system	system;
 	t_editor	editor;
@@ -126,7 +129,7 @@ typedef struct s_rain {
 	t_graph		graph;
 }	t_rain;
 
-// Listed error types
+/*Listed error types*/
 typedef enum e_error
 {
 	BAD_ARGS,
