2c0f790b9dc1c5c8e0ace29b0d735d4982f3b0e7
anselnettles <aviholai@student.hive.fi>
2022-11-30 13:19:17 +0200

    New vertical line function, vline()

--- src/utilities_graphic.c
+++ src/utilities_graphic.c
@@ -6,15 +6,15 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:35:22 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/21 09:47:37 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:13:40 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
-//A pixel drawing function for the SDL surface, created to make the rendering
-//process more simpler.
-void	pixel_put(t_graph *g, int x_source, int y_source, int c)
+/*	A pixel drawing function for the SDL surface, created to make the rendering
+	process more simpler. */
+void	pixel_put(t_graph *g, int x_source, int y_source, int colour)
 {
 	int	x;
 	int	y;
@@ -23,16 +23,45 @@ void	pixel_put(t_graph *g, int x_source, int y_source, int c)
 	x = (x_source * g->scale);
 	y = (y_source * g->scale);
 	w = g->width;
-	((uint32_t *)g->surf->pixels)[(x++) + (y * w)] = (uint32_t)c;
+	((uint32_t *)g->surf->pixels)[(x++) + (y * w)] = (uint32_t)colour;
 	if (g->scale == 2)
 	{
-		((uint32_t *)g->surf->pixels)[x + ((y++) * w)] = (uint32_t)c;
-		((uint32_t *)g->surf->pixels)[(x--) + (y * w)] = (uint32_t)c;
-		((uint32_t *)g->surf->pixels)[x + (y * w)] = (uint32_t)c;
+		((uint32_t *)g->surf->pixels)[x + ((y++) * w)] = (uint32_t)colour;
+		((uint32_t *)g->surf->pixels)[(x--) + (y * w)] = (uint32_t)colour;
+		((uint32_t *)g->surf->pixels)[x + (y * w)] = (uint32_t)colour;
 	}
 }
 
-//'sdl_loop()' keeps Simple Direct MediaLayer's PollEvent consistently running.
+/*	'Vline()' (vertical line) function draws a line of three colors on the
+ 	graphical window. */
+void	vline(t_graph *g, int x_source, int y_source1, int y_source2)
+{
+	uint32_t	*pix;
+	int			y;
+	int			y1;
+	int			y2;
+
+	pix = (uint32_t *)g->surf->pixels;
+	y1 = clamp(y_source1, 0, HEIGHT - 1);
+	y2 = clamp(y_source2, 0, HEIGHT - 1);
+	if (y2 == y1)
+		pix[y1 * WIDTH + x_source] = MIDDLE_COLOUR;
+	else if (y2 > y1)
+	{
+		pix[y1 * WIDTH + x_source] = TOP_COLOUR;
+		y = y1 + 1;
+		while (y < y2)
+		{
+			y++;
+			pix[y * WIDTH + x_source] = MIDDLE_COLOUR;
+		}
+		pix[y2 * WIDTH + x_source] = BOTTOM_COLOUR;
+	}
+}
+
+/*
+'sdl_loop()' keeps Simple Direct MediaLayer's PollEvent consistently running.
+*/
 void	sdl_loop(t_rain *rain)
 {
 	while (ENDLESS)
