e261cdc5d53995c812a37f099a1defe272321418
Ansel "Ami" Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2022-12-01 22:50:36 +0200

    New math functions: overlap() and vxs()

--- src/utilities_math.c
+++ src/utilities_math.c
@@ -12,6 +12,18 @@
 
 #include "library.h"
 
+//Determine whether the two number ranges overlap.
+int	overlap(int a0, int a1, int b0, int b1)
+{
+	return (min(a0, a1) <= max(b0, b1) && min(b0, b1) <= max(a0, a1));
+}
+
+//Vector cross product.
+int	vxs(int x0, int y0, int x1, int y1)
+{
+	return ((x0) * (y1) - (x1) * (y0));
+}
+
 //Return the greater of the applied values
 int	max(int a, int b)
 {
