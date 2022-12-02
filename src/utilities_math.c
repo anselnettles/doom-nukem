3379adc88f46333b2353a639f5688089a62d8d04
Ansel "Ami" Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2022-12-02 13:33:01 +0200

    Readjusted the order.

--- src/utilities_math.c
+++ src/utilities_math.c
@@ -12,18 +12,6 @@
 
 #include "library.h"
 
-//Determine whether the two number ranges overlap.
-int	overlap(int a0, int a1, int b0, int b1)
-{
-	return (min(a0, a1) <= max(b0, b1) && min(b0, b1) <= max(a0, a1));
-}
-
-//Vector cross product.
-int	vxs(int x0, int y0, int x1, int y1)
-{
-	return ((x0) * (y1) - (x1) * (y0));
-}
-
 //Return the greater of the applied values
 int	max(int a, int b)
 {
@@ -46,4 +34,16 @@ int	min(int a, int b)
 int	clamp(int a, int lower, int upper)
 {
 	return (min(max(a, lower), upper));
+}
+
+//Determine whether the two number ranges overlap.
+int	overlap(int a0, int a1, int b0, int b1)
+{
+	return (min(a0, a1) <= max(b0, b1) && min(b0, b1) <= max(a0, a1));
+}
+
+//Vector cross product.
+int	vxs(int x0, int y0, int x1, int y1)
+{
+	return ((x0) * (y1) - (x1) * (y0));
 }
