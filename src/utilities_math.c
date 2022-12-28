d4c109eca673ea8429f57c7b85c1372626954c48
anselnettles <aviholai@student.hive.fi>
2022-12-28 10:21:21 +0200

    Commented out unneeded math functions, for now. Added a degree to radius function.

--- src/utilities_math.c
+++ src/utilities_math.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:50:06 by aviholai          #+#    #+#             */
-/*   Updated: 2022/11/30 17:22:04 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/12/28 10:00:02 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,23 @@ int	clamp(int a, int lower, int upper)
 	return (min(max(a, lower), upper));
 }
 
+double	deg_to_rad(double degrees)
+{
+	return (degrees * (double)M_PI / 180.0);
+}
+
+/*
 //Determine whether the two number ranges overlap.
 int	overlap(int a0, int a1, int b0, int b1)
 {
 	return (min(a0, a1) <= max(b0, b1) && min(b0, b1) <= max(a0, a1));
 }
+*/
 
+/*
 //Vector cross product.
 int	vxs(int x0, int y0, int x1, int y1)
 {
 	return ((x0) * (y1) - (x1) * (y0));
 }
+*/
