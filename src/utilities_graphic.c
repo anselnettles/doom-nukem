0f14e2779beb4c3b8aa73ecb9007c0841a7b4fb1
anselnettles <aviholai@student.hive.fi>
2023-01-11 15:09:25 +0200

    New clean't up vline() format. The texture_color() still needs to be fixed.

--- src/utilities_graphic.c
+++ src/utilities_graphic.c
@@ -6,14 +6,14 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:35:22 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/11 14:26:31 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/11 15:08:32 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "library.h"
 
 //WIP.
-static uint32_t	txtrclr(t_rain *r, int y_position)
+static uint32_t	texture_color(t_rain *r, int value)
 {
 	SDL_Surface	*texture;
 	//uint32_t	*color;
@@ -21,8 +21,10 @@ static uint32_t	txtrclr(t_rain *r, int y_position)
 	int			scale_x;
 
 	texture = NULL;
-	scale_y = 256 * y_position / 64;
+	scale_y = 256 * value / 64;
 	scale_x = 256 * r->graph.raycast.wall_texture_xoffset / 64;
+	if (value >= 0x000001)
+		return (value);
 	if (r->player.compass == NORTH)
 		texture = r->texture.file[0];
 	else if (r->player.compass == EAST)
@@ -34,19 +36,17 @@ static uint32_t	txtrclr(t_rain *r, int y_position)
 	}
 	else if (r->player.compass == WEST)
 		texture = r->texture.file[3];
-	else
-		return (WALL_PRINT);
 	//color = ((uint32_t *)texture->pixels)[scale_x + (scale_y * 256)];
 	//											printf("%d ", color);
 	//r->graph->surf->pixels[100 + (100 * r->graph.width)] = r->texture->file[0]->pixels[1 * (1 * 256)];
-	return (WALL_PRINT);
+	return (0x8f0f0f);
 }
 
 //	A pixel drawing function for the SDL surface, created to make the rendering
 //	process more simpler.
 //	Applies a multiplication of the needed pixels as by the scaling toggle,
 //	one pixel for 1X scale, four pixels for 2X scale and nine pixels for 3X.
-//	The scanline visual effect ('g->sl') is applied on every second pixel
+//	The scanline visual effect ('g->scanline') is applied on every second pixel
 //	color of the column to create an aesthetic alternating scanline effect.
 void	pixel_put(t_graph *g, int x_src, int y_src, uint32_t color)
 {
@@ -59,19 +59,19 @@ void	pixel_put(t_graph *g, int x_src, int y_src, uint32_t color)
 	x = (x_src * g->scale);
 	y = (y_src * g->scale);
 	wth = g->width;
-	pix[(x) + (y * wth)] = color << (g->sl * (y_src % 2));
+	pix[(x) + (y * wth)] = color << (g->scanline * (y_src % 2));
 	if (g->scale >= 2)
 	{
-		pix[(x + 1) + ((y) * wth)] = color << (g->sl * (y_src % 2));
+		pix[(x + 1) + ((y) * wth)] = color << (g->scanline * (y_src % 2));
 		pix[(x) + ((y + 1) * wth)] = color;
 		pix[(x + 1) + ((y + 1) * wth)] = color;
 		if (g->scale == 3)
 		{
-			pix[(x + 2) + ((y) * wth)] = color << (g->sl * (y_src % 2));
+			pix[(x + 2) + ((y) * wth)] = color << (g->scanline * (y_src % 2));
 			pix[(x + 2) + ((y + 1) * wth)] = color;
-			pix[(x) + ((y + 2) * wth)] = color << (g->sl * (y_src % 2));
-			pix[(x + 1) + ((y + 2) * wth)] = color << (g->sl * (y_src % 2));
-			pix[(x + 2) + ((y + 2) * wth)] = color << (g->sl * (y_src % 2));
+			pix[(x) + ((y + 2) * wth)] = color << (g->scanline * (y_src % 2));
+			pix[(x +1) + ((y +2) * wth)] = color << (g->scanline * (y_src % 2));
+			pix[(x +2) + ((y +2) * wth)] = color << (g->scanline * (y_src % 2));
 		}
 	}
 }
@@ -82,11 +82,14 @@ void	pixel_put(t_graph *g, int x_src, int y_src, uint32_t color)
 //	The main color (the  middle color) is handled through the proxy function
 //	'txtrclr()' (texture color) to choose a pixel color from the texture
 //	image files.
-//	The value 'i' stands for the texture's saved y position which is
-//	incremented each time a pixel for the main color is planted.
+//	The integer 'value' stands for the texture's saved y position which is
+//	incremented each time a pixel for the main color is planted, or
+//	alternatively for a HEX color value. The bit shifted color values ('<<')
+//	simply adjust a little bit for a differing effect for the start and end
+//	point of each column line.
 //	The scanline visual effect ('r->graph.sl') is applied on every second
 //	main color pixel to create an aesthetic alternating scanline effect. 
-void	vline(t_rain *r, t_corf sta, t_corf end, int i)
+void	vline(t_rain *r, t_corf start, t_corf end, int value)
 {
 	uint32_t	*pix;
 	int			y;
@@ -95,21 +98,22 @@ void	vline(t_rain *r, t_corf sta, t_corf end, int i)
 	int			wth;
 
 	pix = r->graph.surf->pixels;
-	y1 = clamp(sta.y, 0, r->graph.height - 1);
-	y2 = clamp(end.y, 0, r->graph.height - 1);
+	y1 = clamp((int)start.y, 0, r->graph.height - 1);
+	y2 = clamp((int)end.y, 0, r->graph.height - 1);
 	wth = r->graph.width;
 	if (y2 == y1)
-		pix[(y1 * wth) + sta.x] = r->graph.top_color;
+		pix[(y1 * wth) + (int)start.x] = value << 3;
 	else if (y2 > y1)
 	{
-		pix[(y1 * wth) + sta.x] = r->graph.top_color;
+		pix[(y1 * wth) + (int)start.x] = value << 3;
 		y = y1 + 1;
 		while (y < y2)
 		{
-			pix[(y * wth) + sta.x] = txtrclr(r,i) << (r->graph.sl * (sta.y % 2));
-			i += r->graph.raycast.wall_texture_yincrement;
+			pix[(y * wth) + (int)start.x] = texture_color(r, value)
+				<< (r->graph.scanline * (y % 2));
+			value += r->graph.raycast.wall_texture_yincrement;
 			y++;
 		}
-		pix[(y2 * wth) + sta.x] = r->graph.bottom_color;
+		pix[(y2 * wth) + (int)start.x] = value << 5;
 	}
 }
