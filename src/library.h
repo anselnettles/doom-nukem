7200ba95be60e8280d01e19bf07a0cd4a3d6ef0d
anselnettles <aviholai@student.hive.fi>
2023-01-12 15:08:02 +0200

    Readjusting colors.

--- src/library.h
+++ src/library.h
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:26:57 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/11 18:23:56 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:06:25 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,11 @@
 # define WEST 4
 
 //GRAPHICAL RENDER COLOR DEFINITIONS
-# define SKY_PRINT 0x76776e
+# define SKY_PRINT 0xad6f43
 # define WALL_PRINT 0x3f373e
-# define FLOOR_PRINT 0x293b3d
+# define FLOOR_PRINT 0x37352f
+# define BLUE_OUTLINE 0x042b49
+# define BROWN_OUTLINE 0x593e1c
 
 // PLAYER MAP COLOR DEFINITIONS
 # define WALL 0xFF772E
