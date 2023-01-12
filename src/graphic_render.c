104efc7cc82de7ab998584fee455ac53f57c1ddb
anselnettles <aviholai@student.hive.fi>
2023-01-12 15:54:18 +0200

    Moved the raycasting functions to a new location (raycasting.c). Shortened variable names regarding raycasting. Shortened the struct names.

--- src/graphic_render.c
+++ src/graphic_render.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 10:05:39 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/12 12:49:37 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:50:30 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 static void	draw_column(t_rain *r, t_location lo, float texture_y)
 {
-	if (r->graph.raycast.slice_height < r->graph.height)
+	if (r->graph.cast.slice_height < r->graph.height)
 	{
-		lo.end_y = (r->graph.height / 2) - (r->graph.raycast.slice_height / 2);
+		lo.end_y = (r->graph.height / 2) - (r->graph.cast.slice_height / 2);
 		vline(r, lo, 0, SKY_PRINT);
 		lo.start_y = lo.end_y + 1;
-		lo.end_y += r->graph.raycast.slice_height;
+		lo.end_y += r->graph.cast.slice_height;
 	}
 	vline(r, lo, texture_y, 0);
 	if (lo.end_y + 1 < r->graph.height)
@@ -31,199 +31,30 @@ static void	draw_column(t_rain *r, t_location lo, float texture_y)
 	}
 }
 
-
 //	There is a magic number (+90) used for applying a longer scale on the
 //	otherwise cubic wall heights.
 void	column_render(t_rain *r, int ray_count)
 {
-	t_raycast	*raycast;
+	t_cast		*cast;
 	t_location	location;
-	float	texture_y;
+	float		texture_y;
 
-	raycast = &r->graph.raycast;
-	raycast->slice_height = ((float)SQUARE_SIZE + 90) / raycast->closest_coll_dist * raycast->plane_distance;
-	raycast->wall_texture_yincrement = ((float)SQUARE_SIZE + 90) / (float)raycast->slice_height;
-	raycast->wall_texture_yoffset = 0;
-	if (raycast->slice_height > r->graph.height)
+	cast = &r->graph.cast;
+	cast->slice_height
+		= ((float)SQUARE_SIZE + 90) / cast->closest_coll * cast->plane_dist;
+	cast->texture_yincrement
+		= ((float)SQUARE_SIZE + 90) / (float)cast->slice_height;
+	cast->texture_yoffset = 0;
+	if (cast->slice_height > r->graph.height)
 	{
-		raycast->wall_texture_yoffset = (raycast->slice_height - r->graph.height) / 2;
-		raycast->slice_height = r->graph.height + 90;
+		cast->texture_yoffset = (cast->slice_height - r->graph.height) / 2;
+		cast->slice_height = r->graph.height + 90;
 	}
-	texture_y = raycast->wall_texture_yoffset * raycast->wall_texture_yincrement;
+	texture_y = cast->texture_yoffset * cast->texture_yincrement;
 	location.start_x = ray_count;
 	location.start_y = 0;
 	location.end_y = r->graph.height;
 	draw_column(r, location, texture_y);
-}
-
-float	ray_collision_distance(t_player *player, t_corf collision)
-{
-	float	distance;
-	float	temp;
-
-	temp = (float)((player->pos_x * player->pos_x) - \
-			(2.0f * player->pos_x * collision.x) + \
-			(collision.x * collision.x)) + \
-			(float)((player->pos_y * player->pos_y) - \
-			(2.0f * player->pos_y * collision.y) + \
-			(collision.y * collision.y));
-	distance = square_root(temp);
-	return (distance);
-}
-
-int	find_hor_coll_point(t_rain *r)
-{
-	if (r->graph.raycast.ray_angle > 0 && r->graph.raycast.ray_angle < 180)
-	{
-		r->graph.raycast.ray_y = (((int)r->player.pos_y >> 6) << 6) - 0.001f;
-		r->graph.raycast.ray_x = r->player.pos_x + \
-			(r->player.pos_y - r->graph.raycast.ray_y) / \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-		r->graph.raycast.offset_y = -SQUARE_SIZE;
-		r->graph.raycast.offset_x = -r->graph.raycast.offset_y / tan(deg_to_rad(r->graph.raycast.ray_angle));
-	}
-	else if (r->graph.raycast.ray_angle > 180 && \
-				r->graph.raycast.ray_angle < 360)
-	{
-		r->graph.raycast.ray_y = (((int)r->player.pos_y >> 6) << 6) + SQUARE_SIZE;
-		r->graph.raycast.ray_x = r->player.pos_x + \
-			(r->player.pos_y - r->graph.raycast.ray_y) / \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-		r->graph.raycast.offset_y = SQUARE_SIZE;
-		r->graph.raycast.offset_x = -r->graph.raycast.offset_y / \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-	}
-	else
-		return (0);
-	return (1);
-}
-
-float	calc_hor_coll_dist(t_rain *r)
-{
-	int		index;
-	float	distance;
-
-	index = 0;
-	while (index < (r->index.y / 2))
-	{
-		r->graph.raycast.map_x = ((int)r->graph.raycast.ray_x) >> 6;
-		r->graph.raycast.map_y = ((int)r->graph.raycast.ray_y) >> 6;
-		if (r->graph.raycast.map_x >= 0 && r->graph.raycast.map_x < (r->index.width / 2)
-				&& r->graph.raycast.map_y >= 0 && r->graph.raycast.map_y < (r->index.y / 2)
-				&& r->stage.grid[r->graph.raycast.map_y][r->graph.raycast.map_x] == '#')
-		{
-			r->graph.raycast.hor_coll_point_x = r->graph.raycast.ray_x;
-			r->corf.x = r->graph.raycast.ray_x;
-			r->corf.y = r->graph.raycast.ray_y;
-			distance = ray_collision_distance(&r->player, r->corf);
-			return (distance);
-		}
-		else
-		{
-			r->graph.raycast.ray_x += r->graph.raycast.offset_x;
-			r->graph.raycast.ray_y += r->graph.raycast.offset_y;
-			index++;
-		}
-	}
-	return (100000);
-}
-
-void	save_horizontal(t_rain *r, float hor_coll_dist)
-{
-	r->graph.raycast.closest_coll_dist = hor_coll_dist;
-	r->graph.raycast.wall_texture_xoffset = (int)r->graph.raycast.hor_coll_point_x % \
-					  SQUARE_SIZE;
-	if (r->graph.raycast.ray_angle > 0 && r->graph.raycast.ray_angle < 180)
-		r->player.compass = NORTH;
-	else
-		r->player.compass = SOUTH;
-}
-
-int	find_ver_coll_point(t_rain *r)
-{
-	if (r->graph.raycast.ray_angle > 90 && r->graph.raycast.ray_angle < 270)
-	{
-		r->graph.raycast.ray_x = (((int)r->player.pos_x >> 6) << 6) - 0.001f;
-		r->graph.raycast.ray_y = r->player.pos_y + \
-			(r->player.pos_x - r->graph.raycast.ray_x) * \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-		r->graph.raycast.offset_x = -SQUARE_SIZE;
-		r->graph.raycast.offset_y = -r->graph.raycast.offset_x * \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-	}
-	else if ((r->graph.raycast.ray_angle > 270 && \
-			r->graph.raycast.ray_angle <= 360) || \
-			(r->graph.raycast.ray_angle >= 0 && r->graph.raycast.ray_angle < 90))
-	{
-		r->graph.raycast.ray_x = (((int)r->player.pos_x >> 6) << 6) + SQUARE_SIZE;
-		r->graph.raycast.ray_y = r->player.pos_y + \
-			(r->player.pos_x - r->graph.raycast.ray_x) * \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-		r->graph.raycast.offset_x = SQUARE_SIZE;
-		r->graph.raycast.offset_y = -r->graph.raycast.offset_x * \
-			tan(deg_to_rad(r->graph.raycast.ray_angle));
-	}
-	else
-		return (0);
-	return (1);
-}
-
-float	calc_ver_coll_dist(t_rain *r)
-{
-	int		index;
-	float	distance;
-
-	index = 0;
-	while (index < (r->index.width / 2))
-	{
-		r->graph.raycast.map_x = ((int)r->graph.raycast.ray_x) >> 6;
-		r->graph.raycast.map_y = ((int)r->graph.raycast.ray_y) >> 6;
-		if (r->graph.raycast.map_x >= 0 && r->graph.raycast.map_x < (r->index.width / 2)
-				&& r->graph.raycast.map_y >= 0 && r->graph.raycast.map_y < (r->index.y / 2)
-			&& r->stage.grid[r->graph.raycast.map_y][r->graph.raycast.map_x] == '#')
-		{
-			r->graph.raycast.ver_coll_point_y = r->graph.raycast.ray_y;
-			r->corf.x = r->graph.raycast.ray_x;
-			r->corf.y = r->graph.raycast.ray_y;
-			distance = ray_collision_distance(&r->player, r->corf);
-			return (distance);
-		}
-		else
-		{
-			r->graph.raycast.ray_x += r->graph.raycast.offset_x;
-			r->graph.raycast.ray_y += r->graph.raycast.offset_y;
-			index++;
-		}
-	}
-	return (100000);
-}
-
-void	save_vertical(t_rain *r, float ver_coll_dist)
-{
-	r->graph.raycast.closest_coll_dist = ver_coll_dist;
-	r->graph.raycast.wall_texture_xoffset = (int)r->graph.raycast.ver_coll_point_y % SQUARE_SIZE;
-	if (r->graph.raycast.ray_angle > 90 && r->graph.raycast.ray_angle < 270)
-		r->player.compass = WEST;
-	else
-		r->player.compass = EAST;
-}
-
-int	raycast(t_rain *r)
-{
-	float	hor_coll_dist;
-	float	ver_coll_dist;
-
-	hor_coll_dist = 100000;
-	ver_coll_dist = 100000;
-	if (find_hor_coll_point(r))
-		hor_coll_dist = calc_hor_coll_dist(r);
-	if (find_ver_coll_point(r))
-		ver_coll_dist = calc_ver_coll_dist(r);
-	if (ver_coll_dist < hor_coll_dist)
-		save_vertical(r, ver_coll_dist);
-	else
-		save_horizontal(r, hor_coll_dist);
-	return (0);
 }
 
 //	Beginning of drawing the three-dimensional space.
@@ -231,22 +62,22 @@ static int	draw_space(t_rain *r)
 {
 	int	ray_count;
 
-	r->graph.raycast.ray_angle = r->player.pos_angle + (FOV / 2);
-	if (r->graph.raycast.ray_angle > 360)
-		r->graph.raycast.ray_angle -= 360;
-	else if (r->graph.raycast.ray_angle < 0)
-		r->graph.raycast.ray_angle += 360;
+	r->graph.cast.ray_angle = r->player.pos_angle + (FOV / 2);
+	if (r->graph.cast.ray_angle > 360)
+		r->graph.cast.ray_angle -= 360;
+	else if (r->graph.cast.ray_angle < 0)
+		r->graph.cast.ray_angle += 360;
 	ray_count = 0;
 	while (ray_count < r->graph.width)
 	{
 		raycast(r);
-		if (r->graph.raycast.closest_coll_dist > 0)
+		if (r->graph.cast.closest_coll > 0)
 			column_render(r, ray_count);
-		r->graph.raycast.ray_angle -= r->graph.raycast.degrees_per_ray;
-		if (r->graph.raycast.ray_angle > 360)
-			r->graph.raycast.ray_angle -= 360;
-		else if (r->graph.raycast.ray_angle < 0)
-			r->graph.raycast.ray_angle += 360;
+		r->graph.cast.ray_angle -= r->graph.cast.degrees_per_ray;
+		if (r->graph.cast.ray_angle > 360)
+			r->graph.cast.ray_angle -= 360;
+		else if (r->graph.cast.ray_angle < 0)
+			r->graph.cast.ray_angle += 360;
 		ray_count++;
 	}
 	return (0);
@@ -325,10 +156,10 @@ int	initialize_media(t_graph *g)
 		if (g->win != NULL || g->surf != NULL)
 		{
 			g->map = PLAYER_MAP;
-			g->raycast.plane_distance = (double)(g->width / 2)
+			g->cast.plane_dist = (double)(g->width / 2)
 				/ tan(deg_to_rad(FOV / 2));
-			g->raycast.degrees_per_column = (double)g->width / (double)FOV;
-			g->raycast.degrees_per_ray = (double)FOV / (double)g->width;
+			g->cast.degrees_per_column = (double)g->width / (double)FOV;
+			g->cast.degrees_per_ray = (double)FOV / (double)g->width;
 			return (0);
 		}
 	}
