d95cb8a6752694d8a922a42416a109e7e1e2759c
Ansel "Ami" Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2023-01-13 11:59:16 +0200

    Move_turn() clean't to be more shorter code. Move_forward_back() now includes the collision check which was removed from controls_keyboard.c.

--- src/utilities_movement.c
+++ src/utilities_movement.c
@@ -12,40 +12,19 @@
 
 #include "library.h"
 
-
-void	raycast_angle_check(t_graph *g)
+void	raycast_angle_check(t_graph *graph)
 {
-	if (g->cast.ray_angle >= 360)
-		g->cast.ray_angle -= 360;
-	else if (g->cast.ray_angle < 0)
-		g->cast.ray_angle += 360;
+	if (graph->cast.ray_angle >= 360)
+		graph->cast.ray_angle -= 360;
+	else if (graph->cast.ray_angle < 0)
+		graph->cast.ray_angle += 360;
 }
 
-void	move_forward_back(t_stage *stage, t_player *p, t_graph *g)
-{
-	if (g->e.key.keysym.sym == SDLK_w || g->e.key.keysym.sym == SDLK_UP)
-	{
-		if (stage->grid[p->collision.loc_y][p->collision.loc_x_pos_offset] != '#')
-			p->pos_x += p->dir_x * (float)p->move_speed;
-		if (stage->grid[p->collision.loc_y_pos_offset][p->collision.loc_x] != '#')
-			p->pos_y += p->dir_y * (float)p->move_speed;
-	}
-	if (g->e.key.keysym.sym == SDLK_s || g->e.key.keysym.sym == SDLK_DOWN)
-	{
-		if (stage->grid[p->collision.loc_y][p->collision.loc_x_neg_offset] != '#')
-			p->pos_x -= p->dir_x * (float)p->move_speed;
-		if (stage->grid[p->collision.loc_y_neg_offset][p->collision.loc_x] != '#')
-			p->pos_y -= p->dir_y * (float)p->move_speed;
-	}
-}
-
-void	collision_check(t_player *p)
+static void	collision_check(t_player *p)
 {
 	int	x_offset;
 	int	y_offset;
 
-	x_offset = 0;
-	y_offset = 0;
 	if (p->dir_x < 0)
 		x_offset = -15;
 	else
@@ -54,31 +33,43 @@ void	collision_check(t_player *p)
 		y_offset = -15;
 	else
 		y_offset = 15;
-	p->collision.loc_x = p->pos_x / 64;
-	p->collision.loc_x_pos_offset = (p->pos_x + x_offset) / 64;
-	p->collision.loc_x_neg_offset = (p->pos_x - x_offset) / 64;
-	p->collision.loc_y = p->pos_y / 64;
-	p->collision.loc_y_pos_offset = (p->pos_y + y_offset) / 64;
-	p->collision.loc_y_neg_offset = (p->pos_y - y_offset) / 64;
+	p->collision.loc_x = (p->pos_x / 64);
+	p->collision.loc_x_pos_offset = ((p->pos_x + x_offset) / 64);
+	p->collision.loc_x_neg_offset = ((p->pos_x - x_offset) / 64);
+	p->collision.loc_y = (p->pos_y / 64);
+	p->collision.loc_y_pos_offset = ((p->pos_y + y_offset) / 64);
+	p->collision.loc_y_neg_offset = ((p->pos_y - y_offset) / 64);
 }
 
-
-void	move_turn(t_player *p, t_graph *g)
+void	move_forward_back(t_stage *stage, t_player *p, SDL_Keycode key)
 {
-	if (g->e.key.keysym.sym == SDLK_a || g->e.key.keysym.sym == SDLK_LEFT)
+	collision_check(p);
+	if (key == SDLK_w || key == SDLK_UP)
 	{
+		if (stage->grid[p->collision.loc_y][p->collision.loc_x_pos_offset] != '#')
+			p->pos_x += p->dir_x * (float)p->move_speed;
+		if (stage->grid[p->collision.loc_y_pos_offset][p->collision.loc_x] != '#')
+			p->pos_y += p->dir_y * (float)p->move_speed;
+	}
+	if (key == SDLK_s || key == SDLK_DOWN)
+	{
+		if (stage->grid[p->collision.loc_y][p->collision.loc_x_neg_offset] != '#')
+			p->pos_x -= p->dir_x * (float)p->move_speed;
+		if (stage->grid[p->collision.loc_y_neg_offset][p->collision.loc_x] != '#')
+			p->pos_y -= p->dir_y * (float)p->move_speed;
+	}
+}
+
+void	move_turn(t_player *p, SDL_Keycode key)
+{
+	if (key == SDLK_a || key == SDLK_LEFT)
 		p->pos_angle += TURN_SPEED;
-		if (p->pos_angle >= 360)
-			p->pos_angle = 0;
-		p->dir_x = (float)cos(deg_to_rad(p->pos_angle));
-		p->dir_y = (float)-sin(deg_to_rad(p->pos_angle));
-	}
-	if (g->e.key.keysym.sym == SDLK_d || g->e.key.keysym.sym == SDLK_RIGHT)
-	{
+	if (key == SDLK_d || key == SDLK_RIGHT)
 		p->pos_angle -= TURN_SPEED;
-		if (p->pos_angle < 0)
-			p->pos_angle = 359;
-		p->dir_x = (float)cos(deg_to_rad(p->pos_angle));
-		p->dir_y = (float)-sin(deg_to_rad(p->pos_angle));
-	}
+	if (p->pos_angle >= 360)
+		p->pos_angle -= 360;
+	if (p->pos_angle < 0)
+		p->pos_angle += 360;
+	p->dir_x = (float)cos(deg_to_rad(p->pos_angle));
+	p->dir_y = (float)-sin(deg_to_rad(p->pos_angle));
 }
