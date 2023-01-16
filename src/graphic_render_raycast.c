545126a620b0d7007353d7c316aaff25f04a7051
anselnettles <aviholai@student.hive.fi>
2023-01-16 15:45:23 +0200

    Overhauling the raycast functions to save more space. Fused together collision distance measurement functions and the angle and compass functions.

--- src/graphic_render_raycast.c
+++ src/graphic_render_raycast.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:49:19 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/16 08:53:49 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/16 15:41:13 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,11 @@ int	find_hor_coll_point(t_rain *r)
 	{
 		r->graph.cast.ray_y = (((int)r->player.pos_y >> 6) << 6) - 0.001f;
 		r->graph.cast.ray_x = r->player.pos_x + \
-			(r->player.pos_y - r->graph.cast.ray_y) / \
-			tan(deg_to_rad(r->graph.cast.ray_angle));
+			(r->player.pos_y - r->graph.cast.ray_y)
+			/ tan(deg_to_rad(r->graph.cast.ray_angle));
 		r->graph.cast.offset_y = -SQUARE_SIZE;
-		r->graph.cast.offset_x = -r->graph.cast.offset_y / tan(deg_to_rad(r->graph.cast.ray_angle));
+		r->graph.cast.offset_x = -r->graph.cast.offset_y
+			/ tan(deg_to_rad(r->graph.cast.ray_angle));
 	}
 	else if (r->graph.cast.ray_angle > 180 && \
 				r->graph.cast.ray_angle < 360)
@@ -52,47 +53,6 @@ int	find_hor_coll_point(t_rain *r)
 	else
 		return (0);
 	return (1);
-}
-
-float	calc_hor_coll_dist(t_rain *r)
-{
-	int		index;
-	float	distance;
-
-	index = 0;
-	while (index < (r->index.y / 2))
-	{
-		r->graph.cast.map_x = ((int)r->graph.cast.ray_x) >> 6;
-		r->graph.cast.map_y = ((int)r->graph.cast.ray_y) >> 6;
-		if (r->graph.cast.map_x >= 0 && r->graph.cast.map_x < (r->index.width / 2)
-				&& r->graph.cast.map_y >= 0 && r->graph.cast.map_y < (r->index.y / 2)
-				&& r->stage.grid[r->graph.cast.map_y][r->graph.cast.map_x] == '#')
-		{
-			r->graph.cast.hor_coll_point_x = r->graph.cast.ray_x;
-			r->corf.x = r->graph.cast.ray_x;
-			r->corf.y = r->graph.cast.ray_y;
-			distance = ray_collision_distance(&r->player, r->corf);
-			return (distance);
-		}
-		else
-		{
-			r->graph.cast.ray_x += r->graph.cast.offset_x;
-			r->graph.cast.ray_y += r->graph.cast.offset_y;
-			index++;
-		}
-	}
-	return (100000);
-}
-
-void	save_horizontal(t_rain *r, float hor_coll_dist)
-{
-	r->graph.cast.closest_coll = hor_coll_dist;
-	r->graph.cast.texture_xoffset = (int)r->graph.cast.hor_coll_point_x % \
-					  SQUARE_SIZE;
-	if (r->graph.cast.ray_angle > 0 && r->graph.cast.ray_angle < 180)
-		r->player.compass = NORTH;
-	else
-		r->player.compass = SOUTH;
 }
 
 int	find_ver_coll_point(t_rain *r)
@@ -124,60 +84,60 @@ int	find_ver_coll_point(t_rain *r)
 	return (1);
 }
 
-float	calc_ver_coll_dist(t_rain *r)
+float	calculate_collision_distance(t_rain *r, int i, int measure, float dist)
 {
-	int		index;
-	float	distance;
-
-	index = 0;
-	while (index < (r->index.width / 2))
+	while (i < (measure / 2))
 	{
 		r->graph.cast.map_x = ((int)r->graph.cast.ray_x) >> 6;
 		r->graph.cast.map_y = ((int)r->graph.cast.ray_y) >> 6;
-		if (r->graph.cast.map_x >= 0 && r->graph.cast.map_x < (r->index.width / 2)
-				&& r->graph.cast.map_y >= 0 && r->graph.cast.map_y < (r->index.y / 2)
+		if (r->graph.cast.map_x >= 0 && r->graph.cast.map_x < r->index.width / 2
+			&& r->graph.cast.map_y >= 0 && r->graph.cast.map_y < r->index.y / 2
 			&& r->stage.grid[r->graph.cast.map_y][r->graph.cast.map_x] == '#')
 		{
-			r->graph.cast.ver_coll_point_y = r->graph.cast.ray_y;
+			if (measure == r->index.y)
+				r->graph.cast.hor_coll_point_x = r->graph.cast.ray_x;
+			else
+				r->graph.cast.ver_coll_point_y = r->graph.cast.ray_y;
 			r->corf.x = r->graph.cast.ray_x;
 			r->corf.y = r->graph.cast.ray_y;
-			distance = ray_collision_distance(&r->player, r->corf);
-			return (distance);
+			dist = ray_collision_distance(&r->player, r->corf);
+			return (dist);
 		}
 		else
 		{
 			r->graph.cast.ray_x += r->graph.cast.offset_x;
 			r->graph.cast.ray_y += r->graph.cast.offset_y;
-			index++;
+			i++;
 		}
 	}
 	return (100000);
 }
 
-void	save_vertical(t_rain *r, float ver_coll_dist)
+int	raycast(t_rain *r, float hor_coll_dist, float ver_coll_dist)
 {
-	r->graph.cast.closest_coll = ver_coll_dist;
-	r->graph.cast.texture_xoffset = (int)r->graph.cast.ver_coll_point_y % SQUARE_SIZE;
-	if (r->graph.cast.ray_angle > 90 && r->graph.cast.ray_angle < 270)
-		r->player.compass = WEST;
-	else
-		r->player.compass = EAST;
-}
-
-int	raycast(t_rain *r)
-{
-	float	hor_coll_dist;
-	float	ver_coll_dist;
-
-	hor_coll_dist = 100000;
-	ver_coll_dist = 100000;
 	if (find_hor_coll_point(r))
-		hor_coll_dist = calc_hor_coll_dist(r);
+		hor_coll_dist = calculate_collision_distance(r, 0, r->index.y, 0);
 	if (find_ver_coll_point(r))
-		ver_coll_dist = calc_ver_coll_dist(r);
+		ver_coll_dist = calculate_collision_distance(r, 0, r->index.width, 0);
 	if (ver_coll_dist < hor_coll_dist)
-		save_vertical(r, ver_coll_dist);
+	{
+		r->graph.cast.closest_coll = ver_coll_dist;
+		r->graph.cast.texture_xoffset
+			= (int)r->graph.cast.ver_coll_point_y % SQUARE_SIZE;
+		if (r->graph.cast.ray_angle > 90 && r->graph.cast.ray_angle < 270)
+			r->player.compass = WEST;
+		else
+			r->player.compass = EAST;
+	}
 	else
-		save_horizontal(r, hor_coll_dist);
+	{
+		r->graph.cast.closest_coll = hor_coll_dist;
+		r->graph.cast.texture_xoffset
+			= (int)r->graph.cast.hor_coll_point_x % SQUARE_SIZE;
+		if (r->graph.cast.ray_angle > 0 && r->graph.cast.ray_angle < 180)
+			r->player.compass = NORTH;
+		else
+			r->player.compass = SOUTH;
+	}
 	return (0);
 }
