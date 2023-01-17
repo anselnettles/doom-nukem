fec81eff1abd605be24999e1d912fb69257bd4e8
anselnettles <aviholai@student.hive.fi>
2023-01-17 19:32:11 +0200

    Work on buffer_to_map() continues.

--- src/parse_file_buffer_to_map.c
+++ src/parse_file_buffer_to_map.c
@@ -6,13 +6,13 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:16:02 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/17 15:44:57 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:25:42 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitter_cold_droplets_in_autumn_rain.h"
 
-static int	check_for_start_position(char b[MAX + 1], t_index *i)
+static char	check_for_start_position(char b[MAX + 1], t_index *i)
 {
 	if (b[i->i] == 'S' || b[i->i + 1] == 'S'
 			|| b[i->i + MAP_WIDTH] == 'S' || b[i->i + MAP_WIDTH + 1] == 'S')
@@ -21,7 +21,7 @@ static int	check_for_start_position(char b[MAX + 1], t_index *i)
 		return (' ');
 }
 
-static int	check_for_walls(char b[MAX + 1], t_index *i)
+static char	check_for_walls(char b[MAX + 1], t_index *i)
 {
 	if (b[i->i] == '#' || b[i->i + 1] == '#'
 			|| b[i->i + MAP_WIDTH] == '#' || b[i->i + MAP_WIDTH + 1] == '#'
@@ -42,9 +42,20 @@ static int	check_for_walls(char b[MAX + 1], t_index *i)
 
 int	buffer_to_map(char b[MAX + 1], t_editor *editor, t_index *i)
 {
-	i->p = 0;
-	editor->map[i->y][i->x] = check_for_walls(b, i);
-	editor->map[i->y][i->x][i->p++] = check_for_start_position(b, i);
+	if (i->i == MAP_WIDTH)
+	{
+		if (b[i->i] != '\n')
+			return (error(BAD_WIDTH));
+		editor->map[i->y][i->x] = '\0';
+		return (NEW_LINE);
+	}
+	else
+	{
+		i->p = 0;
+		editor->map[i->y][i->x] = check_for_walls(b, i);
+		editor->map[i->y][i->x][i->p++] = check_for_start_position(b, i);
+		if (editor->map[i->y][i->x] == '#' && editor->map[i->y][i->x][1] == 'S')
+			return (error(BAD_WALL);
+	}
 	return (0);
-}
-
+} 
