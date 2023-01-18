7aadeb65407607c1fce7f34b5c4d602cf980c826
anselnettles <aviholai@student.hive.fi>
2023-01-18 12:21:12 +0200

    Now saving wall and start position location to the new three dimensional array.

--- src/parse_file_buffer_to_map.c
+++ src/parse_file_buffer_to_map.c
@@ -6,17 +6,21 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:16:02 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/17 19:25:42 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:08:20 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitter_cold_droplets_in_autumn_rain.h"
 
-static char	check_for_start_position(char b[MAX + 1], t_index *i)
+static char	check_for_start(char b[MAX + 1], t_index *i, t_editor *editor)
 {
 	if (b[i->i] == 'S' || b[i->i + 1] == 'S'
-			|| b[i->i + MAP_WIDTH] == 'S' || b[i->i + MAP_WIDTH + 1] == 'S')
+			|| b[i->i + MAP_WIDTH + 1] == 'S' || b[i->i + MAP_WIDTH + 2] == 'S')
+	{
+		editor->start_y = i->y;
+		editor->start_x = i->x;
 		return ('S');
+	}
 	else
 		return (' ');
 }
@@ -24,17 +28,17 @@ static char	check_for_start_position(char b[MAX + 1], t_index *i)
 static char	check_for_walls(char b[MAX + 1], t_index *i)
 {
 	if (b[i->i] == '#' || b[i->i + 1] == '#'
-			|| b[i->i + MAP_WIDTH] == '#' || b[i->i + MAP_WIDTH + 1] == '#'
+			|| b[i->i + MAP_WIDTH + 1] == '#' || b[i->i + MAP_WIDTH + 2] == '#'
 			|| b[i->i] == '/' || b[i->i + 1] == '/'
-			|| b[i->i + MAP_WIDTH] == '/' || b[i->i + MAP_WIDTH + 1] == '/'
+			|| b[i->i + MAP_WIDTH + 1] == '/' || b[i->i + MAP_WIDTH + 2] == '/'
 			|| b[i->i] == '\\' || b[i->i + 1] == '\\'
-			|| b[i->i + MAP_WIDTH] == '\\' || b[i->i + MAP_WIDTH + 1] == '\\'
+			|| b[i->i + MAP_WIDTH + 1] == '\\' || b[i->i + MAP_WIDTH + 2] == '\\'
 			|| b[i->i] == '*' || b[i->i + 1] == '*'
-			|| b[i->i + MAP_WIDTH] == '*' || b[i->i + MAP_WIDTH + 1] == '*'
+			|| b[i->i + MAP_WIDTH + 1] == '*' || b[i->i + MAP_WIDTH + 2] == '*'
 			|| b[i->i] == '[' || b[i->i + 1] == '['
-			|| b[i->i + MAP_WIDTH] == '[' || b[i->i + MAP_WIDTH + 1] == '['
+			|| b[i->i + MAP_WIDTH + 1] == '[' || b[i->i + MAP_WIDTH + 2] == '['
 			|| b[i->i] == ']' || b[i->i + 1] == ']'
-			|| b[i->i + MAP_WIDTH] == ']' || b[i->i + MAP_WIDTH + 1] == ']')
+			|| b[i->i + MAP_WIDTH + 1] == ']' || b[i->i + MAP_WIDTH + 2] == ']')
 		return ('#');
 	else
 		return (' ');
@@ -42,20 +46,19 @@ static char	check_for_walls(char b[MAX + 1], t_index *i)
 
 int	buffer_to_map(char b[MAX + 1], t_editor *editor, t_index *i)
 {
-	if (i->i == MAP_WIDTH)
+	if ((i->i > 0) && ((i->i == MAP_WIDTH) || (i->i % (MAP_WIDTH + 1) == MAP_WIDTH)))
 	{
 		if (b[i->i] != '\n')
 			return (error(BAD_WIDTH));
-		editor->map[i->y][i->x] = '\0';
+		editor->map[i->y][i->x][i->p] = '\0';
 		return (NEW_LINE);
 	}
 	else
 	{
-		i->p = 0;
-		editor->map[i->y][i->x] = check_for_walls(b, i);
-		editor->map[i->y][i->x][i->p++] = check_for_start_position(b, i);
-		if (editor->map[i->y][i->x] == '#' && editor->map[i->y][i->x][1] == 'S')
-			return (error(BAD_WALL);
+		editor->map[i->y][i->x][0] = check_for_walls(b, i);
+		editor->map[i->y][i->x][1] = check_for_start(b, i, editor);
+		if (editor->map[i->y][i->x][0] == '#' && editor->map[i->y][i->x][1] == 'S')
+			return (error(BAD_WALL));
 	}
 	return (0);
 } 
