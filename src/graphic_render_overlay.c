14f44bc0861163fab9e22299c6ef3713ffaedc6b
anselnettles <aviholai@student.hive.fi>
2023-01-18 12:18:24 +0200

    A memory error, so functions in hold for now.

--- src/graphic_render_overlay.c
+++ src/graphic_render_overlay.c
@@ -6,12 +6,12 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:08:33 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/17 11:10:30 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:11:20 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitter_cold_droplets_in_autumn_rain.h"
-
+/*
 static void	droplet(t_rain *r, int x, int y)
 {
 	pixel_put(&r->graph, x, y, 0xC1BCCB);
@@ -25,10 +25,11 @@ static void	droplet(t_rain *r, int x, int y)
 	pixel_put(&r->graph, x, y + 11, 0xD4D0DE);
 	pixel_put(&r->graph, x, y + 12, 0xD4D0DE);
 	pixel_put(&r->graph, x, y + 13, 0xD4D0DE);
-}
-
+}*/
+/*
 void	draw_overlay(t_rain *r)
 {
+	(void) r;
 	static int	y[24] = {10, 90, 50, 180, 80, 250, 140, 320, 220, 150,
 		30, 40, 32, 450, 23, 53, 423, 54, 4, 12, 234, 123, 153, 231};
 	static int	x[24] = {10, 90, 20, 80, 30, 70, 40, 60, 50, 110, 120,
@@ -36,8 +37,8 @@ void	draw_overlay(t_rain *r)
 	int			i;
 
 	i = 0;
-	while (i++ < 24)
-		droplet(r, x[i], y[i]++);
+	//while (i++ < 24)
+	//	droplet(r, x[i], y[i]++);
 	i = 0;
 	while (i++ < 24)
 	{
@@ -47,4 +48,4 @@ void	draw_overlay(t_rain *r)
 			x[i] = rand() % 639 + 1;
 		}
 	}
-}
+}*/
