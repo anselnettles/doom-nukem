ac145575b07efea5db0f26c352f4c0603e256fc2
anselnettles <aviholai@student.hive.fi>
2023-01-20 10:29:43 +0200

    Removed a needless 'void' call. Wrote a little comment.

--- src/graphic_render_overlay.c
+++ src/graphic_render_overlay.c
@@ -6,11 +6,17 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:08:33 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/18 15:57:56 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/20 10:08:55 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitter_cold_droplets_in_autumn_rain.h"
+
+//This is a very very poorly optimized, simple rain overlay effect.
+//It is what the bitter cold autumn droplets are. It symbolizes the
+//anxiety and dread of my HIVE studies.
+//By using static integers in draw_overlay(), we can memorize the position
+//of the rain drops.
 
 static void	droplet(t_rain *r, int x, int y)
 {
@@ -29,7 +35,6 @@ static void	droplet(t_rain *r, int x, int y)
 
 void	draw_overlay(t_rain *r)
 {
-	(void) r;
 	static int	y[24] = {10, 90, 50, 180, 80, 250, 140, 320, 220, 150,
 		30, 40, 32, 450, 23, 53, 423, 54, 4, 12, 234, 123, 153, 231};
 	static int	x[24] = {10, 90, 20, 80, 30, 70, 40, 60, 50, 110, 120,
