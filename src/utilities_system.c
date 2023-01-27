827bd21dc96b4cf72423af157b09405f1ab2d7cb
anselnettles <aviholai@student.hive.fi>
2023-01-27 18:19:05 +0200

    Complete overhaul of project structure started. Added all Dofidog's 'libft' library references to here, instead. Renamed all references to 't_rain' over to 't_drown'. Fixed reference to header file.

--- src/utilities_system.c
+++ src/utilities_system.c
@@ -6,11 +6,11 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:50:06 by aviholai          #+#    #+#             */
-/*   Updated: 2023/01/16 09:03:37 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/01/27 18:02:43 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "bitter_cold_droplets_in_autumn_rain.h"
+#include "drowning.h"
 
 //	Ft_memset() (memory set) copies the argument 'len' amount of argument value
 //	'c' to a newly created char string 's' pointed by the argument '*b'.
@@ -63,13 +63,216 @@ size_t	ft_strlen(const char *s)
 //	Sdl_loop() keeps Simple Direct MediaLayer's PollEvent constantly
 //	running and checks for control calls while rerendering the graphical
 //	view.
-void	sdl_loop(t_rain *rain)
+void	sdl_loop(t_drown *drown)
 {
 	while (ENDLESS)
 	{
-		keyboard_input(rain);
-		render(rain);
-		if (SDL_PollEvent(&rain->graph.e) && rain->graph.e.type == SDL_QUIT)
+		keyboard_input(drown);
+		render(drown);
+		if (SDL_PollEvent(&drown->graph.e) && drown->graph.e.type == SDL_QUIT)
 			break ;
 	}
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c && *s)
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*str;
+	int		len;
+
+	len = 0;
+	str = (char *)malloc(sizeof(*str) * ft_strlen(src) + 1);
+	if (!str)
+		return (NULL);
+	while (src[len])
+	{
+		str[len] = src[len];
+		len++;
+	}
+	str[len] = '\0';
+	return (str);
+}
+
+void	ft_memdel(void **ap)
+{
+	if (!ap)
+		return ;
+	if (*ap && ap)
+	{
+		free(*ap);
+		*ap = NULL;
+	}
+}
+
+void	ft_strdel(char **as)
+{
+	ft_memdel((void **)as);
+}
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = ft_memalloc(len + 1);
+	if (!str || !s)
+		return (NULL);
+	while (len)
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+		len--;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	str = ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!s1 || !s2 || !str)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(*str) * size + 1);
+	if (!str)
+		return (NULL);
+	while (i <= size)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	return (str);
+}
+
+int	searchnewline(char **s, char **line, int fd, int ret)
+{
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	while (s[fd][i] != '\n' && s[fd][i] != '\0')
+		i++;
+	if (s[fd][i] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, i);
+		tmp = ft_strdup(s[fd] + i + 1);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	else if (s[fd][i] == '\0')
+	{
+		if (ret == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+	}
+	return (1);
+}
+
+int	get_next_line(const int fd, char **line)
+{
+	static char	*s[MAX_FD];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
+
+	if (fd < 0 || !line || fd > MAX_FD)
+		return (-1);
+	ret = read(fd, buf, BUFF_SIZE);
+	while (ret > 0)
+	{
+		buf[ret] = '\0';
+		if (s[fd] == NULL)
+			s[fd] = ft_strnew(1);
+		tmp = ft_strjoin(s[fd], buf);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (ft_strchr(buf, '\n'))
+			break ;
+		ret = read(fd, buf, BUFF_SIZE);
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+		return (0);
+	return (searchnewline(s, line, fd, ret));
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	if (!str)
+		return ;
+	write(1, str, ft_strlen(str));
+}
+
+void	ft_putstr_fd(char const *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+void	ft_putendl(char *str)
+{
+	ft_putstr(str);
+	ft_putchar('\n');
+}
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+
+void	ft_exit(char *str)
+{
+	ft_putendl(str);
+	exit(-1);
 }
