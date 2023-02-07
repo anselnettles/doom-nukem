b86474eb5b0a21c0638c8d5f4530345489bd8480
anselnettles <aviholai@student.hive.fi>
2023-02-07 16:36:30 +0200

    Applied time measurement to 'sdl_loop()'.

--- src/utilities_system.c
+++ src/utilities_system.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:50:06 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/06 10:03:12 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/07 16:31:16 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,30 @@
 //	Sdl_loop() keeps Simple Direct MediaLayer's PollEvent constantly
 //	running and checks for control calls while rerendering the graphical
 //	view.
-void	sdl_loop(t_drown *data)
+void	sdl_loop(t_drown *d)
 {
-	while (data->play_state == PLAY)
+	while (d->play_state == PLAY)
 	{
-		while(SDL_PollEvent(&data->event) != 0)
+		d->system.current_time = SDL_GetTicks();
+		d->system.delta_time = (d->system.current_time - d->system.last_time);
+		if ((d->system.current_time / 1000) >= d->system.second)
 		{
-			if (data->event.type == SDL_QUIT)
-				data->play_state = EXIT;
-			else if (data->event.type == SDL_KEYDOWN)
-				deal_key(data->event.key.keysym.sym, data);
-			else if (data->event.type == SDL_MOUSEMOTION)
-				deal_mouse(data);
+				d->system.second++;
+				d->index.i++;
 		}
-		render(data);
+		while (SDL_PollEvent(&d->event) != 0)
+		{
+			if (d->event.type == SDL_QUIT)
+				d->play_state = EXIT;
+			else if (d->event.type == SDL_KEYDOWN)
+				deal_key(d->event.key.keysym.sym, d);
+			else if (d->event.type == SDL_MOUSEMOTION)
+				deal_mouse(d);
+		}
+		if (d->system.delta_time)
+		{
+			render(d);
+			d->system.last_time = d->system.current_time;
+		}
 	}
 }
