0827d8c344e3f5102a0d7b57043763b5f5a3c064
anselnettles <aviholai@student.hive.fi>
2023-02-07 19:38:11 +0200

    Created a looping animation rig for the right arm sprite. More work to be had, for sure.

--- src/utilities_system.c
+++ src/utilities_system.c
@@ -6,26 +6,41 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:50:06 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/07 16:31:16 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:37:05 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "drowning.h"
 
-//	Sdl_loop() keeps Simple Direct MediaLayer's PollEvent constantly
-//	running and checks for control calls while rerendering the graphical
-//	view.
-void	sdl_loop(t_drown *d)
+static void	animations(t_drown *d)
 {
-	while (d->play_state == PLAY)
-	{
+	if (d->system.current_time % 600 > 0 && d->system.current_time % 600 < 600)
+		d->gfx.frame.right_arm = 1;
+	if (d->system.current_time % 1200 > 0 && d->system.current_time % 1200 < 600)
+		d->gfx.frame.right_arm = 2;
+	if (d->system.current_time % 1800 > 0 && d->system.current_time % 1800 < 600)
+		d->gfx.frame.right_arm = 1;
+	if (d->system.current_time % 2400 > 0 && d->system.current_time % 2400 < 600)
+		d->gfx.frame.right_arm = 0;
+}
+
+static void	track_time(t_drown *d)
+{
 		d->system.current_time = SDL_GetTicks();
 		d->system.delta_time = (d->system.current_time - d->system.last_time);
 		if ((d->system.current_time / 1000) >= d->system.second)
-		{
 				d->system.second++;
-				d->index.i++;
-		}
+		animations(d);
+}
+
+//	Sdl_loop() keeps Simple Direct MediaLayer's PollEvent constantly
+//	running and checks for control calls while rerendering the graphical
+//	view.
+void		sdl_loop(t_drown *d)
+{
+	while (d->play_state == PLAY)
+	{
+		track_time(d);
 		while (SDL_PollEvent(&d->event) != 0)
 		{
 			if (d->event.type == SDL_QUIT)
