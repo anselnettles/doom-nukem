85bd65797d079415a1ca3dc074d952a5814af60b
anselnettles <aviholai@student.hive.fi>
2023-02-08 16:45:01 +0200

    Removed old unnecessary code, for now.

--- src/controls_keyboard.c
+++ src/controls_keyboard.c
@@ -6,42 +6,13 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:16:37 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/06 09:51:28 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/08 16:08:49 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "drowning.h"
+
 /*
-static void	toggle_scale(t_drown *d)
-{
-	if (d->gfx.scale == 1)
-		d->gfx.scale = 2;
-	else
-		d->gfx.scale = 1;
-	//d->graph.width = (WIDTH * d->graph.scale);
-	//d->graph.height = (HEIGHT * d->graph.scale);
-	//SDL_SetWindowSize(d->graph.win, d->graph.width, d->graph.height);
-	//d->graph.surf = SDL_GetWindowSurface(d->graph.win);
-	//d->graph.cast.plane_dist = (double)(d->graph.width / 2)
-	//	/ tan(deg_to_rad(FOV / 2));
-	//d->graph.cast.degrees_per_column = (double)d->graph.width / (double)FOV;
-	//d->graph.cast.degrees_per_ray = (double)FOV / (double)d->graph.width;
-
-	//RETURN TO INITIALIZE_MEDIA() INSTEAD?
-}
-
-static void	quit_program(t_drown *d)
-{
-//	SDL_FreeSurface(d->gfx.texture[0]);
-//	SDL_FreeSurface(d->gfx.texture[1]);
-//	SDL_FreeSurface(d->gfx.texture[2]);
-//	SDL_FreeSurface(d->gfx.texture[3]);
-	SDL_DestroyWindow(d->gfx.window);
-	(void)d;
-	SDL_Quit();
-	exit(0);
-}
-
 static void	keyboard_second_batch(t_drown *d, SDL_Keycode key)
 {
 	if (key == SDLK_ESCAPE)
