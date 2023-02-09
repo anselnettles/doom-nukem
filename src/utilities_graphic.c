c5e43cd642ab42a7ed371a41adaa213040364dbc
anselnettles <aviholai@student.hive.fi>
2023-02-09 17:33:15 +0200

    Work on 'gfx_write()' and 'draw_letters()' started.

--- src/utilities_graphic.c
+++ src/utilities_graphic.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:35:22 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/08 19:34:58 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/09 17:32:28 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,69 +38,58 @@ int	pixel_put(t_gfx *gfx, int x, int y, uint32_t color)
 	return (0);
 }
 
-/*
-//'Texture_color()' picks the correct RGB color from the correct texture
-//file's pixels, corresponding to the parameter texture_y location.
-static uint32_t	txtr(t_drown *d, float texture_y)
-{
-	SDL_Surface	*texture;
-	uint32_t	color;
-	int			scale_y;
-	int			scale_x;
 
-	texture = NULL;
-	scale_y = (int)(156 * texture_y) / SQUARE_SIZE;
-	scale_x = (256 * d->graph.cast.texture_xoffset) / SQUARE_SIZE;
-	if (d->player.compass == NORTH)
-		texture = d->graph.texture[0];
-	else if (d->player.compass == EAST)
-		texture = d->graph.texture[1];
-	else if (d->player.compass == SOUTH)
-		texture = d->graph.texture[2];
-	else if (d->player.compass == WEST)
-		texture = d->graph.texture[3];
-	color = ((uint32_t *)texture->pixels)[scale_x + (scale_y * texture->w)];
-	return (color);
+int	draw_letter(t_gfx *gfx, int x)
+{
+	int	y;
+	int	temp_y;
+
+	y = ((gfx->height / 4) * 3);
+	x += (gfx->width / 4);
+	//goal_y = gfx->y + LETTER_HEIGHT;
+	goal_x = gfx->x + LETTER_WIDTH;
+	while (gfx->y)
+	{
+		while (gfx->x < goal_x)
+		{
+			if (gfx->sprite.letters[gfx->y][gfx->x])
+			{
+				if (pixel_put(gfx, x, y,
+							gfx->sprite.letters[gfx->y][gfx->x]) == ERROR)
+					return (ERROR);
+			}
+			x += (gfx->scale);
+			gfx->x++;
+		}
+		y += (gfx->scale);
+		gfx->y++;
+		x = (gfx->width / 4);
+		gfx->x = 0;
+	}
+	return (0);
 }
 
-//	'Vline()' (vertical line) function draws a line of three colors on the
-//	graphical window. The start and end points are of different color only
-//	to make the polygonal shapes easier to view.
-//	The main color (the  middle color) is handled through the proxy function
-//	'txtrclr()' (texture color) to choose a pixel color from the texture
-//	image files.
-//	The integer 'value' stands for the texture's saved y position which is
-//	incremented each time a pixel for the main color is planted, or
-//	alternatively for a HEX color value. The bit shifted color values ('<<')
-//	simply adjust a little bit for a differing effect for the start and end
-//	point of each column line.
-//	The scanline visual effect ('r->graph.sl') is applied on every second
-//	main color pixel to create an aesthetic alternating scanline effect. 
-void	vline(t_drown *d, t_location lo, float y, uint32_t color)
+int	gfx_write(t_gfx *gfx, char *str)
 {
-	uint32_t	*pix;
+	int	i;
+	int	x;
 
-	pix = d->graph.surf->pixels;
-	lo.y1 = clamp((int)lo.start_y, 0, d->graph.height - 1);
-	lo.y2 = clamp((int)lo.end_y, 0, d->graph.height - 1);
-	if (lo.y2 == lo.y1)
-		pix[(lo.y1 * d->graph.width) + (int)lo.start_x] = BLUE_OUTLINE;
-	else if (lo.y2 > lo.y1)
+	i = 0;
+	x= 0;
+	while(str[i] != '\0')
 	{
-		pix[(lo.y1 * d->graph.width) + (int)lo.start_x] = BLUE_OUTLINE;
-		lo.y = lo.y1 + 1;
-		while (lo.y < lo.y2)
+		if ((str[i] >= ',' && str[i] <= '.') || (str[i] >= 'A' && str[i] <= 'Z')
+				|| str[i] == ' ' || str[i] == '!')
 		{
-			if (color == 0)
-				pix[((lo.y++) * d->graph.width) + (int)lo.start_x] = txtr(d, y);
-			else
-			{
-				pix[((lo.y++) * d->graph.width) + (int)lo.start_x] = color++;
-				if (d->graph.scale >= 2)
-					pix[((lo.y++) * d->graph.width) + (int)lo.start_x] = color;
-			}
-			y += (float)d->graph.cast.texture_yincrement;
+			gfx->x = str[i] * LETTER_WIDTH;
+			gfx->y = 0;
+			if (draw_letter(gfx, x) == ERROR)
+				return (error(GFX_WRITE_ERROR));
 		}
-		pix[(lo.y2 * d->graph.width) + (int)lo.start_x] = BROWN_OUTLINE;
+		else
+			return (error(GFX_WRITE_ERROR);
+		i++;
+		x += LETTER_WIDTH;
 	}
-}*/
+	return (0);
+}
