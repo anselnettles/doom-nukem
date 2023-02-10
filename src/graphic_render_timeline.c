01df5d51dd8d5dcafdfc742477639cc833e3faf3
anselnettles <aviholai@student.hive.fi>
2023-02-10 14:58:24 +0200

    Applying new graphical timeline variables. Practicing.

--- src/graphic_render_timeline.c
+++ src/graphic_render_timeline.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 12:34:59 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/10 12:38:31 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/10 12:43:50 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	string_timeline(t_drown *d)
 {
-	if (d->system.time < 3200)
+	if (d->system.time > 600 && d->system.time < 3600)
 	{
-		if (gfx_write(&d->gfx, "HELLO WORLD.") == ERROR)
+		if (gfx_write(&d->gfx, "...!!? N-NO! NO! NO NO NO NO!") == ERROR)
 			return (ERROR);
 	}
-	if (d->system.time > 3200 && (d->system.time % 1000 > 0 && d->system.time % 1000 < 500))
+	if (d->system.time > 4000) //&& d->system.time < 8200)
 	{
-		if (gfx_write(&d->gfx, "\"A QUICK BROWN FOX'S JUMP OVERS THE LAZY DOG!??\"") == ERROR)
+		if (gfx_write(&d->gfx, "THIS CAN'T BE HAPPENING! I'M GONNA DROWN!") == ERROR)
 			return (ERROR);
 	}
 	return (0);
