d9c86479d55032d0df6c5d950ca6b3d85dbb2523
anselnettles <aviholai@student.hive.fi>
2023-02-10 14:58:12 +0200

    Applying new graphical timeline variables.

--- src/graphic_render_animations.c
+++ src/graphic_render_animations.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 10:28:04 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/09 18:46:49 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/10 14:56:04 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,8 +60,20 @@ static void	right_arm_loop(t_drown *d)
 		d->gfx.frame.right_arm = 0;
 }
 
+static void	flow_adjustment(t_drown *d)
+{
+	if (d->system.time % 2400 > 0 && d->system.time % 2400 < 600)
+	{
+	//	if (d->system.time % 1200 >= 0 && d->system.time % 1200 <= 25)
+			d->gfx.flow_y_adjust = (d->system.time % 200 / 60);
+	//	else if (d->system.time % 1200 >= 75 && d->system.time % 1200 <= 100)
+	//		d->gfx.flow_y_adjust = 0;
+	}
+}
+
 int			animation_loop(t_drown *d)
 {
 	right_arm_loop(d);
+	flow_adjustment(d);
 	return (0);
 }
