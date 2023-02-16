4dc050cdc6251674be5896d71a64d9f1afee4889
anselnettles <aviholai@student.hive.fi>
2023-02-16 16:10:41 +0200

    Moved HUD calls to a new file, 'graphic_render_hug.c'.

--- src/graphic_render_overlay.c
+++ src/graphic_render_overlay.c
@@ -6,76 +6,11 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:08:33 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/16 11:21:42 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:09:26 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "drowning.h"
-
-static void	draw_timer_bubbles(t_index *index, t_gfx *gfx, int s)
-{
-	int	start_y = index->y - (55 * s);
-	int	start_x = index->x + (10 * s);
-	int x;
-	int y;
-	int	gfx_y;
-	int	gfx_x;
-	int	i = gfx->frame.bubble;
-
-	while (13 - i)
-	{
-		y = start_y + (i % 4 * 13 * s);
-		gfx_y = 0;
-		while (gfx_y < 12)
-		{
-			x = start_x + (i % 3 * 13 * s);
-			gfx_x = 0;
-			while (gfx_x < 12)
-			{
-				if (gfx->sprite.bubble[gfx_y][gfx_x])
-					pixel_put(gfx, x, y + (gfx->frame.timer % 2),
-								gfx->sprite.bubble[gfx_y][gfx_x]);
-				x += s;
-				gfx_x++;
-			}
-			y += s;
-			gfx_y++;
-			gfx_x = 0;
-		}
-		i++;
-	}
-}
-
-static int	draw_timer_bottle(t_index *index, t_gfx *gfx, int s)
-{
-	gfx->f = gfx->frame.timer;
-	gfx->x = 0;
-	gfx->y = 0;
-	index->y = (MARGIN * s);
-	index->x = (gfx->width - (60 * s) - (MARGIN * s));
-	while ((gfx->y) < (90))
-	{
-		while ((gfx->x) < (60))
-		{
-			if (gfx->sprite.timer[gfx->f][gfx->y][gfx->x])
-			{
-				if (pixel_put(gfx, index->x, index->y,
-						gfx->sprite.timer
-						[gfx->f][gfx->y][gfx->x]) == ERROR)
-					return (ERROR);
-			}
-			index->x += s;
-			gfx->x++;
-		}
-		index->y += s;
-		gfx->y++;
-		index->x = ((gfx->width - (60 * s) - MARGIN * s));
-		gfx->x = 0;
-	}
-	draw_timer_bubbles(index, gfx, s);
-	return (0);
-}
-
 
 static int	draw_right_arm(t_index *index, t_gfx *gfx, int s)
 {
@@ -112,7 +47,7 @@ int	render_overlay(t_drown *d)
 		draw_color_filter(&d->gfx);
 	if (d->gfx.frame.bubble >= 13)
 		SDL_FillRect(d->gfx.screen, NULL, 0);
-	if (draw_timer_bottle(&d->index, &d->gfx, d->gfx.scale) == ERROR)
+	if (render_hud(&d->index, &d->gfx, d->gfx.scale) == ERROR)
 		return (ERROR);
 	if (string_timeline(d) == ERROR)
 		return (ERROR);
