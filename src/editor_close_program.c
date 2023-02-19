e574c76455e83adf0e61880231858f4eded02e70
anselnettles <aviholai@student.hive.fi>
2023-02-19 14:09:06 +0200

    Fixed some school norm errors.

--- src/editor_close_program.c
+++ src/editor_close_program.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   editor_close_program.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturto                                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: tturto                                   #+#    #+#             */
+/*   Updated: 2023/02/19 13:43:18 by aviholai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "drowning.h"
 
-void    close_program(t_gfx *gfx)
+void	close_program(t_gfx *gfx)
 {
-    //free map or map_temp
-    SDL_DestroyRenderer(gfx->renderer);
-    gfx->renderer = NULL;
-    SDL_DestroyWindow(gfx->window);
-    gfx->window = NULL;
-    (void)gfx;
-    SDL_Quit();
-    exit(-1);
+	//free map or map_temp
+	SDL_DestroyRenderer(gfx->renderer);
+	gfx->renderer = NULL;
+	SDL_DestroyWindow(gfx->window);
+	gfx->window = NULL;
+	(void)gfx;
+	SDL_Quit();
+	exit(-1);
 }
