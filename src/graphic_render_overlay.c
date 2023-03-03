2d5a5cfbe05af7d0c4e9a7d876e2935da96e559a
anselnettles <aviholai@student.hive.fi>
2023-03-03 14:06:39 +0200

    Underwater wave effect overlay just about working.

--- src/graphic_render_overlay.c
+++ src/graphic_render_overlay.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:08:33 by aviholai          #+#    #+#             */
-/*   Updated: 2023/02/27 16:43:34 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/03/03 14:05:32 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,124 @@ static int	draw_right_arm(t_index *index, t_gfx *gfx, int s)
 	return (0);
 }
 
+static int	underwater_effect(t_drown *d, t_gfx *gfx, int scale)
+{
+	uint32_t	*color;
+	int			i;
+	int			toggle;
+	
+	color = gfx->screen->pixels;
+	gfx->x = 0;
+	gfx->y = 0;
+	i = 0;
+	(void) d;
+	toggle = FALSE;
+	while (gfx->y < gfx->height)
+	{
+		while (gfx->x < gfx->width)
+		{
+			if (pixel_put(gfx, gfx->x, gfx->y, color[(gfx->x + i) + (gfx->y * gfx->width)]) == ERROR)
+				write(1, "yo", 2);
+			gfx->x++;
+		}
+		gfx->y++;
+		gfx->x = 0;
+		if (toggle == FALSE && (gfx->y / 10) % 2)
+			i++;
+		if (toggle == TRUE && !(gfx->y / 10 % 2))
+			i--;
+		if (i >= 10)
+			toggle = TRUE;
+		if (i <= 0)
+		{
+			toggle = FALSE;
+			gfx->y += 10;
+		}
+	}
+	return (0);
+}
+
+/*
+static int	underwater_effect(t_drown *d, t_gfx *gfx, int scale)
+{
+	uint32_t	temp[10];
+	uint32_t	*color;
+	int			i;
+	int			toggle;
+	int			reverse;
+
+	(void)scale;
+	*temp = 0;
+	color = gfx->screen->pixels;
+	
+	i = 0;//d->system.time % 2;
+	toggle = FALSE;
+	reverse = FALSE;
+	gfx->x = 0;
+	gfx->y = 0;
+	while (gfx->y < gfx->height)
+	{
+		while (gfx->x <= gfx->width && gfx->x >= 0)
+		{
+			if (toggle == FALSE)
+			{
+				temp[i] = color[gfx->x + (gfx->y * gfx->width)];
+				
+				if (reverse == FALSE)		
+					gfx->x += scale;
+				else
+					gfx->x -= scale;
+			
+				i++;
+
+				if (i >= 9)
+				{
+					toggle = TRUE;
+					i = 0;
+				}
+			}
+			if (toggle == TRUE)
+			{
+				pixel_put(gfx, gfx->x, gfx->y, temp[i]);
+
+				if (reverse == FALSE)
+					gfx->x += scale;
+				else
+					gfx->x -= scale;
+
+				i++;
+
+				if (i >= 9)
+				{
+					toggle = FALSE;
+					//i = 0;
+				}
+			}
+		}
+		if (i >= 8)
+		{
+			if (reverse == TRUE)
+				reverse = FALSE;
+			else
+				reverse = TRUE;
+		}
+	//	else
+	//	{
+			//i++;
+			gfx->y += scale;
+			//reverse = FALSE;
+	//	}
+		gfx->x = 0;
+		if (reverse == TRUE)
+			gfx->x = gfx->width;
+	}
+	return (0);
+}*/
+
 int	render_overlay(t_drown *d)
 {
+	if (underwater_effect(d, &d->gfx, d->gfx.scale) == ERROR)
+		return (ERROR);
 	if (draw_right_arm(&d->index, &d->gfx, d->gfx.scale) == ERROR)
 		return (ERROR);
 	if (d->system.filters == TRUE)
