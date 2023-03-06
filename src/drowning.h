38185a49f68590d6fde1e316814c5a4616aa2275
anselnettles <aviholai@student.hive.fi>
2023-03-06 16:57:08 +0200

    Readjustments of animation functions.

--- src/drowning.h
+++ src/drowning.h
@@ -6,7 +6,7 @@
 /*   By: tpaaso <tpaaso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:26:57 by aviholai          #+#    #+#             */
-/*   Updated: 2023/03/06 15:45:21 by tpaaso           ###   ########.fr       */
+/*   Updated: 2023/03/06 16:54:16 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,26 +103,7 @@
 # define CHAIN 14
 # define CHAIN_WIDTH 12
 
-//	Texture 'A', 0:		Floor texture.	Frames: 0.			Size: 64 x 64
-//	Texture 'B', 1:		Wall texture.	Frames: 0.			Size: 64 x 64
-//	Texture 'C', 2:		Pillar.			Frames: 0.			Size: 64 x 64
-//	Texture 'D', 3:		Crate texture.	Frames: 0.			Size: 64 x 64
-//	Texture 'E', 4:		Sky texture.	Frames: 0.			Size: 720 x 240
-//	Texture 'F', 5:		Right arm.		Frames: 0 to 4.		Size: 250 x 238
-//	Texture 'G', 6:		Letters.		Frames: 0.			Size: 728 x 20
-//	Texture 'H', 7:		Harpoon.		Frames: 0 to 6.		Size: 64 x 64
-//	Texture 'I', 8:		Bottle.			Frames: 0 to 3.		Size: 38 x 64
-//	Texture 'J', 9:		Timer.			Frames: 0 to 9.		Size: 60 x 90
-//	Texture 'K', 10:	Bubble.			Frames: 0.			Size: 12 x 12
-//	Texture 'L', 11:	Ammo.			Frames:	0 to 1.		Size: 32 x 45
-//	Texture 'M', 12		Monster.		Placeholder.
-//	Texture 'N', 13		Algae texture.	Frames: 0 to 3.		Size: 64 x 64
-//	Texture 'O', 14:	Rope chain.		Frames: 0 to 2.		Size: 16 x 64
-
-//Map Editor definitions
-// # define BUF_SIZE 6000000	/*remove*/
-# define SCREEN_W 1280
-# define SCREEN_H 800
+//	MAP EDITOR DEFINITIONS
 # define IMG1_CATHETUS 12
 # define IMG2_CATHETUS 80
 # define IMG3_CATHETUS 70
@@ -465,6 +446,7 @@ int			render_hud(t_index *index, t_gfx *gfx, int scale);
 int			render_overlay(t_drown *d);
 int			render(t_drown *d);
 void		render_thread(t_drown *data);
+void		right_arm_loop(t_drown *d);
 void		rotate_player(int key, t_player *player);
 void		scale_window(t_gfx *gfx);
 void		sdl_loop(t_drown *drown);
@@ -488,6 +470,10 @@ int			ft_diagonal_4(t_wall *wall, t_ray *ray);
 void		crouch(t_drown *data);
 void		init_dda(t_ray *ray, t_wall *wall, t_dda *dda);
 void   		algo_dda(t_ray *ray, t_wall *wall, t_dda *dda);
+void		flow_adjustment(t_drown *d);
+void		overworld_sprite_loop(t_drown *d);
+void		hud_animation_loop(t_drown *d);
+
 
 //dont remove before final build. it is used to test if map values are changed correctly
 void    	testing_print_map(t_drown *data, t_editor_images *images);
