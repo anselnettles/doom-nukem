e65fcf4aa02ef81007a1eebacc7fec3c4560884f
Ansel "Ami" Nettles | 网ˢᵗ <aviholai@student.hive.fi>
2023-03-10 03:18:34 +0200

    New parsing set for identity 'Q', 16: Left_arm.

--- src/editor_parse_textures_malloc_batches.c
+++ src/editor_parse_textures_malloc_batches.c
@@ -12,22 +12,23 @@
 
 #include "drowning.h"
 
-/*
-    Texture 'A', 0:		Floor texture.	Frames: 0.			Size: 64 x 64
-    Texture 'B', 1:		Wall texture.	Frames: 0.			Size: 64 x 64
-    Texture 'C', 2:		Pillar.			Frames: 0.			Size: 64 x 64
-    Texture 'D', 3:		Crate texture.	Frames: 0.			Size: 64 x 64
-    Texture 'E', 4:		Sky texture.	Frames: 0.			Size: 720 x 240
-    Texture 'F', 5:		Right arm.		Frames: 0 to 4.		Size: 250 x 238
-    Texture 'G', 6:		Letters.		Frames: 0.			Size: 728 x 20
-    Texture 'H', 7:		Harpoon.		Frames: 0 to 6.		Size: 64 x 64
-    Texture 'I', 8:		Bottle.			Frames: 0 to 3.		Size: 38 x 64
-    Texture 'J', 9:		Timer.			Frames: 0 to 9.		Size: 60 x 90
-    Texture 'K', 10:	Bubble.			Frames: 0.			Size: 12 x 12
-    Texture 'L', 11:	Ammo.			Frames:	0 to 1.		Size: 32 x 45
-    Texture 'M', 12		Monster.		Placeholder.
-    Texture 'N', 13		Algae texture.	Frames: 0 to 3.		Size: 64 x 64
-*/
+//	Texture 'A', 0:		Floor texture.	Frames: 0.			Size: 64 x 64
+//	Texture 'B', 1:		Wall texture.	Frames: 0.			Size: 64 x 64
+//	Texture 'C', 2:		Pillar.			Frames: 0.			Size: 64 x 64
+//	Texture 'D', 3:		Crate texture.	Frames: 0.			Size: 64 x 64
+//	Texture 'E', 4:		Sky texture.	Frames: 0.			Size: 720 x 240
+//	Texture 'F', 5:		Right arm.		Frames: 0 to 4.		Size: 250 x 238
+//	Texture 'G', 6:		Letters.		Frames: 0.			Size: 728 x 20
+//	Texture 'H', 7:		Harpoon.		Frames: 0 to 6.		Size: 64 x 64
+//	Texture 'I', 8:		Bottle.			Frames: 0 to 3.		Size: 38 x 64
+//	Texture 'J', 9:		Timer.			Frames: 0 to 9.		Size: 60 x 90
+//	Texture 'K', 10:	Bubble.			Frames: 0.			Size: 12 x 12
+//	Texture 'L', 11:	Ammo.			Frames:	0 to 1.		Size: 32 x 45
+//	Texture 'M', 12		Monster.		Placeholder.
+//	Texture 'N', 13:	Algae texture.	Frames: 0 to 3.		Size: 64 x 64
+//	Texture 'O', 14:	Rope chain.		Frames: 0 to 2.		Size: 16 x 64
+//	Texture 'P', 15:	Transition.		Frames: 0 to 2.		Size: 642 x 48
+//	Texture 'Q', 16:	Left arm.		Frames: 0 to 2.		Size: 182 x 492
 static void	memory_allocate_textures_fourth_batch(t_gfx *gfx)
 {
 	gfx->texture[11].frame[0].pixels
@@ -56,6 +57,12 @@ static void	memory_allocate_textures_fourth_batch(t_gfx *gfx)
 		= (uint32_t *)malloc(sizeof(uint32_t) * (642 * 48));
 	gfx->texture[15].frame[2].pixels
 		= (uint32_t *)malloc(sizeof(uint32_t) * (642 * 48));
+	gfx->texture[16].frame[0].pixels
+		= (uint32_t *)malloc(sizeof(uint32_t) * (182 * 492));
+	gfx->texture[16].frame[1].pixels
+		= (uint32_t *)malloc(sizeof(uint32_t) * (182 * 492));
+	gfx->texture[16].frame[2].pixels
+		= (uint32_t *)malloc(sizeof(uint32_t) * (182 * 492));
 }
 
 static void	memory_allocate_textures_third_batch(t_gfx *gfx)
