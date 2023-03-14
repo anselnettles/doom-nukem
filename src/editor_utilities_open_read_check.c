0fe95a907fc93ada568e56db49027dd2f2f59176
anselnettles <aviholai@student.hive.fi>
2023-03-14 16:48:41 +0200

    Reformatting some parameters in editor functions in order to 'quit_program()'.

--- src/editor_utilities_open_read_check.c
+++ src/editor_utilities_open_read_check.c
@@ -6,7 +6,7 @@
 /*   By: tturto <tturto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 17:25:51 by tturto            #+#    #+#             */
-/*   Updated: 2023/03/10 17:31:02 by tturto           ###   ########.fr       */
+/*   Updated: 2023/03/14 16:37:39 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 /*
 	These functions are only called by read_map().
 */
-void	open_check(int *fd, char *map_file)
+int	open_check(int *fd, char *map_file)
 {
 	*fd = open(map_file, O_RDONLY);
 	if (*fd < 0)
 	{
 		tt_errors("read_map: open() fail.");
-		exit(-1);
+		return (1);
 	}
+	return (0);
 }
 
-void	read_check(int fd, int *ret, char *buf, int size)
+int	read_check(int fd, int *ret, char *buf, int size)
 {
 	*ret = read(fd, buf, size);
 	if (*ret <= 0)
 	{
 		tt_errors("read_map: read() fail.");
-		exit(-1);
+		return (1);
 	}
+	return (0);
 }
