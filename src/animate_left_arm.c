ce3b3279fe891e7f38aa4b070c8c206ae52b91c7
anselnettles <aviholai@student.hive.fi>
2023-03-22 15:11:51 +0200

    When turning on the lantern now has a flickering effect.

--- src/animate_left_arm.c
+++ src/animate_left_arm.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:14:11 by aviholai          #+#    #+#             */
-/*   Updated: 2023/03/22 14:41:06 by aviholai         ###   ########.fr       */
+/*   Updated: 2023/03/22 15:11:30 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,13 @@ void	lantern_on_flicker(t_drown *d)
 
 	if (d->system.time >= duration)
 		duration++;
-	printf("duration: %d \n", duration);
-	if (duration >= 20)
+	if ((duration >= 2 && duration <= 3) || (duration >= 5 && duration <= 6)
+		|| (duration >= 8 && duration <= 9) || (duration >= 12 && duration <= 13)
+			|| (duration >= 14 && duration <= 16) || duration >= 17)
+		d->gfx.lantern = (TRUE + 0.1);
+	else
+		d->gfx.lantern = TURN_ON;
+	if (duration >= 130)
 	{
 		duration = 0;
 		d->gfx.lantern = TRUE;
@@ -28,6 +33,6 @@ void	lantern_on_flicker(t_drown *d)
 
 void	left_arm_loop(t_drown *d)
 {
-	if (d->gfx.lantern == TURN_ON)
+	if (d->gfx.lantern <= TURN_ON && d->gfx.lantern > TRUE)
 		lantern_on_flicker(d);
 }
